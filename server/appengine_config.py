c8f81ecc0bec81f7409f885588e755619c07f5d0
Sharad Vikram <sharad.vikram@gmail.com>
2014-06-07 18:17:21 -0700

    updated appengine config to include path

--- server/appengine_config.py
+++ server/appengine_config.py
@@ -2,16 +2,23 @@
 App Engine config
 
 """
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'gaenv'))
 
 
 def gae_mini_profiler_should_profile_production():
-    """Uncomment the first two lines to enable GAE Mini Profiler on production for admin accounts"""
+    """Uncomment the first two lines to enable GAE
+    Mini Profiler on production for admin accounts"""
     # from google.appengine.api import users
     # return users.is_current_user_admin()
     return False
 
 
 def webapp_add_wsgi_middleware(app):
+    """
+    Middleware for WSGI
+    """
     from google.appengine.ext.appstats import recording
     app = recording.appstats_wsgi_middleware(app)
     return app
