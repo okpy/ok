fbd27059a3846f0cea71d808a33fd523c2010a3c
Stephen Martinis <moowiz2020@gmail.com>
2014-06-07 22:12:22 -0700

    Added DB stuff

--- server/run.py
+++ server/run.py
@@ -1,5 +1,13 @@
+"""
+Sets up the path and stuff for anything running this.
+
+You can run "python -i run.py" to have a sort of console.
+"""
 import os
 import sys
 
-sys.path.insert(1, os.path.join(os.path.abspath('.'), 'env/lib/python2.7/site-packages'))
-import app
+import dev_appserver
+dev_appserver.fix_sys_path()
+
+sys.path.insert(1, os.path.join(os.path.abspath('.'), 'gaenv'))
+import app #pylint: disable=W0611
