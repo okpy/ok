b6cedf7945f62fccbe6b0642550052eaba42cca8
Sharad Vikram <sharad.vikram@gmail.com>
2014-06-07 18:22:55 -0700

    added environment linking to gaenv

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
+if env | grep -q ^VIRTUAL_ENV=
+then
+    echo '$VIRTUAL_ENV is defined'
+else
+    echo 'You need to set $VIRTUAL_ENV before continuing'
+    exit  11
+fi
 
 server/app/generate_keys.py
 
 pip install -r requirements.txt
 
 ln -s ../../hooks/pre-commit.py .git/hooks/pre-commit
+
+echo 'Linking environments'
+linkenv $VIRTUAL_ENV/lib/python2.7/site-packages gaenv  1>/dev/null
