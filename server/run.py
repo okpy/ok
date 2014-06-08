62994e789ebee6b7c67e87b02d316b8439e3d7a8
Stephen Martinis <moowiz2020@gmail.com>
2014-06-07 22:24:43 -0700

    Oh, didn't need that

--- server/run.py
+++ server/run.py
@@ -6,8 +6,5 @@ You can run "python -i run.py" to have a sort of console.
 import os
 import sys
 
-import dev_appserver
-dev_appserver.fix_sys_path()
-
 sys.path.insert(1, os.path.join(os.path.abspath('.'), 'gaenv'))
 import app #pylint: disable=W0611
