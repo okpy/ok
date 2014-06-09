f227958a821a64e41d83131f9293ea9dc1a69002
Stephen Martinis <moowiz2020@gmail.com>
2014-06-09 00:30:47 -0700

    Worked on tests. Still need to fix the base

--- server/tests/test_simple_db.py
+++ server/tests/test_simple_db.py
@@ -5,9 +5,9 @@ simple_db_tests.py
 
 This module runs basic tests on the MySQL database.
 """
-from basetest import BaseTestCase
+import basetest
 
-class TrivialTestCase(BaseTestCase): #pylint: disable=R0904
+class TrivialTestCase(basetest.BaseTestCase): #pylint: disable=R0904
     """
     Performs extremely simple database operations
     """
@@ -16,24 +16,24 @@ class TrivialTestCase(BaseTestCase): #pylint: disable=R0904
         """
         Creates test user and deletes it
         """
-        db = self.app.models.db
+        database = self.app.models.db
         model = self.app.models
         constants = self.app.constants
 
         test_user = model.User("sharad@sharad.com", "cs61a-tt",
                                constants.ADMIN_ROLE, "Sharad", "Vikram")
-        db.session.add(test_user)
-        db.session.commit()
+        database.session.add(test_user)
+        database.session.commit()
 
-        query_user = model.User.query.filter_by(email=
+        query_user = model.User.query.filter_by(email= #pylint: disable=no-member
                                                 "sharad@sharad.com").first()
 
         self.assertTrue(query_user is test_user)
 
-        db.session.delete(test_user)
-        db.session.commit()
+        database.session.delete(test_user)
+        database.session.commit()
 
-        query_user = model.User.query.filter_by(email=
+        query_user = model.User.query.filter_by(email= #pylint: disable=no-member
                                                 "sharad@sharad.com").first()
 
         self.assertTrue(query_user is None)
