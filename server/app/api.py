fb54e9dcbdc1062504a08f0b3387c24b38ca1730
Stephen Martinis <moowiz2020@gmail.com>
2014-06-09 23:29:21 -0700

    I'm smart

--- server/app/api.py
+++ server/app/api.py
@@ -82,7 +82,7 @@ class AssignmentAPI(MethodView, APIResource):
     """
     @classmethod
     def get_model(cls):
-        return models.User
+        return models.Assignment
 
 class SubmissionAPI(MethodView, APIResource):
     """
@@ -90,7 +90,7 @@ class SubmissionAPI(MethodView, APIResource):
     """
     @classmethod
     def get_model(cls):
-        return models.User
+        return models.Submission
 
 def register_api(view, endpoint, url, primary_key='id', pk_type='int'):
     """
