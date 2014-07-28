2f941a285d68d112a43a3b43d4d4637967ae00a2
Sharad Vikram <sharad.vikram@gmail.com>
2014-07-28 00:34:46 -0700

    added authentication decorator

---
+++ server/app/auth.py
@@ -0,0 +1,43 @@
+from google.appengine.api import memcache
+
+from app import app
+from app import models
+from app.api import API_PREFIX, create_api_response, handle_error
+
+from flask import request
+from functools import wraps
+import requests
+
+GOOGLE_API_URL = "https://www.googleapis.com/oauth2/v1/userinfo"
+
+def requires_authentication(func):
+    """Decorator that determines which user made the request and passes it as a keyword argument"""
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        if 'access_token' not in request.args:
+            return create_api_response(401,
+                                       "access token required for this method")
+        access_token = request.args['access_token']
+        email = memcache.get(access_token) # pylint: disable=no-member
+        if not email:
+            response = requests.get(GOOGLE_API_URL, params={
+                "access_token": access_token
+            }).json()
+            if 'error' in response:
+                return create_api_response(401,
+                                           "invalid access token")
+            email = response['email']
+            memcache.set(access_token, email, time=60) # pylint: disable=no-member
+        users = list(models.User.query().filter(models.User.email == email))
+        if len(users) == 0:
+            return create_api_response(401, "user doesn't exist")
+        user = users[0]
+        return func(*args, user=user, **kwargs)
+    return decorated
+
+
+@app.route("%s/me" % API_PREFIX)
+@handle_error
+@requires_authentication
+def authenticate(user=None):
+    return create_api_response(200, "success", user)
