0c092fcc98957422000b170d484a4cd7c4ed2148
Sharad Vikram <sharad.vikram@gmail.com>
2014-07-28 01:00:34 -0700

    made fixes

--- server/app/auth.py
+++ server/app/auth.py
@@ -10,7 +10,9 @@ import requests
 
 GOOGLE_API_URL = "https://www.googleapis.com/oauth2/v1/userinfo"
 
-def requires_authentication(func):
+MC_NAMESPACE = "access-token"
+
+def requires_authenticated_user(func):
     """Decorator that determines which user made the request and passes it as a keyword argument"""
     @wraps(func)
     def decorated(*args, **kwargs):
@@ -18,7 +20,8 @@ def requires_authentication(func):
             return create_api_response(401,
                                        "access token required for this method")
         access_token = request.args['access_token']
-        email = memcache.get(access_token) # pylint: disable=no-member
+        mc_key = "%s-%s" % (MC_NAMESPACE, access_token)
+        email = memcache.get(mc_key) # pylint: disable=no-member
         if not email:
             response = requests.get(GOOGLE_API_URL, params={
                 "access_token": access_token
@@ -26,8 +29,11 @@ def requires_authentication(func):
             if 'error' in response:
                 return create_api_response(401,
                                            "invalid access token")
+            if 'email' not in response:
+                return create_api_response(401,
+                                           "email doesn't exist")
             email = response['email']
-            memcache.set(access_token, email, time=60) # pylint: disable=no-member
+            memcache.set(mc_key, email, time=60) # pylint: disable=no-member
         users = list(models.User.query().filter(models.User.email == email))
         if len(users) == 0:
             return create_api_response(401, "user doesn't exist")
@@ -38,6 +44,6 @@ def requires_authentication(func):
 
 @app.route("%s/me" % API_PREFIX)
 @handle_error
-@requires_authentication
+@requires_authenticated_user
 def authenticate(user=None):
     return create_api_response(200, "success", user)
