eedb22b1be419130ffc4a349c3ec4b83879b44bd
Albert Wu <albert12132@gmail.com>
2014-07-30 20:23:04 -0700

    Make proposed testing format with demo assignment

--- client/demo_assignments/hw1_tests.py
+++ client/demo_assignments/hw1_tests.py
@@ -1,33 +1,92 @@
 """Tests for hw1 demo assignment."""
 
-TEST_INFO = {
-    'assignment': 'hw1',
-    'imports': ['from hw1 import *'],
-}
+assignment = {
+  'name': 'hw1',
+  'imports': ['from hw1 import *'],
+  'version': '1.0',
 
-TESTS = [
+  # Specify tests that should not be locked
+  'no_lock': {
+  },
 
+  'tests': [
     # Test square
     {
-        'name': ('Q1', 'q1', '1'),
-        'suites': [
-            [
-                ['square(4)', '16'],
-                ['square(-5)', '25'],
+      # The first name is the "official" name.
+      'name': ['Q1', 'q1', '1'],
+      # No explicit point value -- each test suite counts as 1 point
+      'suites': [
+        [
+          {
+            'type': 'code',     # Code question.
+            'input': 'square(4)',
+            'output': ['16'],   # List of outputs, even if only one
+          },
+          {
+            'type': 'concept',  # Concept question.
+            'input': """
+            What type of input does the square function take?
+            """,
+            'output': [
+              # Denote multiple choice with a list, rather than
+              # a string.
+              [
+                'number',         # Correct choice comes first.
+                'string',
+                'None',
+              ]
             ],
+          },
+          {
+            # If type is omitted, default type is 'code'.
+            'input': """
+            x = -5
+            square(-5)
+            """,
+            # Last line in a multiline input is used as the prompt.
+            'output': ['25'],
+            # Additional statuses can be included here.
+            'status': {
+              'lock': False,
+            }
+          },
         ],
+      ],
     },
-
-
     # Test double
     {
-        'name': ('Q2', 'q2', '2'),
-        'suites': [
-            [
-                ['double(4)', '8'],
-                ['double(-5)', '-10'],
-            ],
+      'name': ['Q2', 'q2', '2'],
+      # Point value specified -- points are partitioned evenly across
+      # suites.
+      'points': 4,
+      'suites': [
+        [
+          {
+            'input': 'double(4)',
+            'output': ['8'],
+          }
         ],
+        [
+          {
+            # Cases with multiple outputs: lines with expected output
+            # are denoted by '$ '.
+            'input': """
+            x = double(4)
+            $ x
+            $ double(x)
+            """,
+            'output': ['8', '16']
+          },
+          {
+            'input': """
+            x = double(2)
+            $ x
+            $ square(x)
+            """,
+            'output': ['4', '16'],
+          },
+        ],
+      ],
     },
-
-]
+  ],
+}
