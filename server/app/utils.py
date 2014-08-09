eb53497825cf12185efee62d41bf8abaa361e110
Stephen Martinis <martiniss@google.com>
2014-08-09 09:43:53 -0700

    Front end kinda working.

--- server/app/utils.py
+++ server/app/utils.py
@@ -1,16 +1,23 @@
 """
 Utility functions used by API and other services
 """
-from flask import jsonify
+from flask import jsonify, request, Response, json
 
 
+#TODO(martinis) somehow having data be an empty list doesn't make it
+# return an empty list, but an empty object.
 def create_api_response(status, message, data=None):
     """Creates a JSON response that contains status code (HTTP),
     an arbitrary message string, and a dictionary or list of data"""
-    response = jsonify(**{
-        'status': status,
-        'message': message,
-        'data': data
-    })
+    print data
+    if request.args.get('format', 'default') == 'raw':
+        response = Response(json.dumps(data))
+        print response.data
+    else:
+        response = jsonify(**{
+            'status': status,
+            'message': message,
+            'data': data
+        })
     response.status_code = status
     return response
