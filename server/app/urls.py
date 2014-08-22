d4c3ea98c1398fb9f7e2600c973041711a44259d
John DeNero <papajohn@gmail.com>
2014-08-22 11:30:26 -0700

    Remove stale TODO and clean up lint errors

--- server/app/urls.py
+++ server/app/urls.py
@@ -2,7 +2,6 @@
 URL dispatch route mappings and error handlers
 """
 from functools import wraps
-import collections
 
 from flask import render_template, session
 
@@ -10,8 +9,6 @@ from app import app
 from app import api
 from app import auth
 from app.constants import API_PREFIX
-from app.needs import Need
-from app.utils import create_api_response
 
 @app.route("/")
 def index():
@@ -21,14 +18,14 @@ def index():
 # Handle 404 errors
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('404.html', error=e), 404
 
 # Handle 500 errors
 @app.errorhandler(500)
 def server_error(e):
-    return render_template('500.html'), 500
+    return render_template('500.html', error=e), 500
 
-def register_api(view, endpoint, url, primary_key='key', pk_type='int:', admin=False):
+def register_api(view, endpoint, url, primary_key='key', pk_type='int:'):
     """
     Registers the given view at the endpoint, accessible by the given url.
     """
@@ -46,8 +43,7 @@ def register_api(view, endpoint, url, primary_key='key', pk_type='int:', admin=F
     app.add_url_rule('%s/<%s%s>' % (url, pk_type, primary_key),
                      view_func=wrapped, methods=['GET', 'PUT', 'DELETE'])
 
-# TODO(denero) Add appropriate authentication requirements
-register_api(api.UserAPI, 'user_api', '/user', admin=True, pk_type='')
-register_api(api.AssignmentAPI, 'assignment_api', '/assignment', admin=True)
+register_api(api.UserAPI, 'user_api', '/user', pk_type='')
+register_api(api.AssignmentAPI, 'assignment_api', '/assignment')
 register_api(api.SubmissionAPI, 'submission_api', '/submission')
 
