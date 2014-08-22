ee5478e7640be02dc4e39ebe2645bce9532f06e7
John DeNero <papajohn@gmail.com>
2014-08-22 10:19:26 -0700

    Simplify needs.py logic

--- server/app/needs.py
+++ server/app/needs.py
@@ -1,6 +1,8 @@
 from app.utils import create_api_response
 
 class Need(object):
+    """A need represents an action taken on an object, such as getting it."""
+
     def __init__(self, action):
         self.action = action
         self.obj = None
@@ -9,13 +11,13 @@ class Need(object):
         self.obj = obj
 
     def api_response(self):
-        if self.obj:
-            obj_name = (self.obj.__name__ if isinstance(self.obj, type) else
-                        self.obj.__class__.__name__)
-        else:
-            obj_name = ""
+        class_name = ""
+        if isinstance(self.obj, type):
+            class_name = self.obj.__name__
+        elif self.obj:
+            class_name = type(self.obj).__name__
         return create_api_response(
-            401, "Don't have permission to {} {}".format(
-                self.action, obj_name))
+            401, "Don't have permission to {} {}".format(self.action,
+                                                         class_name))
 
 
