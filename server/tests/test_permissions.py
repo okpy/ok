70b186bb1718e45dfe582fc8f977ed6a447453d6
John DeNero <papajohn@gmail.com>
2014-08-26 20:39:34 -0700

    Remove JSON file and place test data in permissions.py

--- server/tests/test_permissions.py
+++ server/tests/test_permissions.py
@@ -11,7 +11,7 @@ from app import models
 from app.needs import Need
 from app.constants import ADMIN_ROLE, STAFF_ROLE #pylint: disable=import-error
 
-from ddt import ddt, file_data, unpack
+from ddt import ddt, data
 
 from google.appengine.ext import ndb
 
@@ -22,21 +22,21 @@ class PermissionsUnitTest(BaseTestCase):
         Returns the accounts you want to exist in your system.
         """
         return {
-            "dummy_student": models.User(
+            "student0": models.User(
                 key=ndb.Key("User", "dummy@student.com"),
                 email="dummy@student.com",
                 first_name="Dummy",
                 last_name="Jones",
                 login="some13413"
             ),
-            "other_student": models.User(
+            "student1": models.User(
                 key=ndb.Key("User", "other@student.com"),
                 email="other@student.com",
                 first_name="Other",
                 last_name="Jones",
                 login="other13413",
             ),
-            "dummy_staff": models.User(
+            "staff": models.User(
                 key=ndb.Key("User", "dummy@staff.com"),
                 email="dummy@staff.com",
                 first_name="Staff",
@@ -52,7 +52,7 @@ class PermissionsUnitTest(BaseTestCase):
                 login="albertoo",
                 role=STAFF_ROLE
             ),
-            "dummy_admin": models.User(
+            "admin": models.User(
                 key=ndb.Key("User", "dummy@admin.com"),
                 email="dummy@admin.com",
                 first_name="Admin",
@@ -63,49 +63,54 @@ class PermissionsUnitTest(BaseTestCase):
             "anon": models.AnonymousUser,
         }
 
+    def enroll(self, student, course):
+        """Enroll student in course."""
+        s = self.accounts[student]
+        s.courses.append(self.courses[course].key)
+        s.put()
+
+    def teach(self, staff, course):
+        """Add staff to the staff of course."""
+        self.courses[course].staff.append(self.accounts[staff].key)
+        self.courses[course].put()
+
     def setUp(self):
         super(PermissionsUnitTest, self).setUp()
         self.accounts = self.get_accounts()
 
         self.courses = {
-                "first": models.Course(),
-                "second": models.Course(),
+            "first": models.Course(),
+            "second": models.Course(),
             }
 
         for course in self.courses.values():
             course.put()
 
-        self.accounts['dummy_student'].courses.append(self.courses['first'].key)
-        self.accounts['dummy_student'].put()
-
-        self.courses['first'].staff.append(self.accounts['dummy_staff'].key)
-        self.courses['first'].put()
-
-        self.accounts['other_student'].courses.append(
-            self.courses['second'].key)
-        self.accounts['other_student'].put()
+        self.enroll("student0", "first")
+        self.enroll("student1", "second")
+        self.teach("staff", "first")
 
         self.assignments = {
             "first": models.Assignment(
                 name="first",
                 points=3,
-                creator=self.accounts['dummy_admin'].key
+                creator=self.accounts["admin"].key
                 ),
             "empty": models.Assignment(
                 name="empty",
                 points=3,
-                creator=self.accounts['dummy_admin'].key
+                creator=self.accounts["admin"].key
                 ),
             }
 
         self.submissions = {
             "first": models.Submission(
-                submitter=self.accounts["dummy_student"].key,
+                submitter=self.accounts["student0"].key,
                 assignment=self.assignments["first"].key,
                 messages="{}"
                 ),
             "second": models.Submission(
-                submitter=self.accounts["other_student"].key,
+                submitter=self.accounts["student1"].key,
                 assignment=self.assignments["first"].key,
                 messages="{}"
                 ),
@@ -120,40 +125,107 @@ class PermissionsUnitTest(BaseTestCase):
         assert self.user
         self.user = None
 
-    @file_data('permissions_tests.json')
-    def testAccess(self, value):
-        user_id, obj, need, result = value
-        self.login(user_id)
+    class PTest(object):
+        """A test of wehther a user can access a model object with need."""
+        def __init__(self, name, user, model, obj, need, output):
+            self.__name__ = name
+            self.input = (user, model, obj, need)
+            self.output = output
 
-        model, args = obj
-        if model == 'User':
-            obj = self.accounts[args]
+    permission_tests = [
+        PTest("student_get_own",
+              "student0", "Submission", "first", "get", True),
+        PTest("stuent_get_other",
+              "student1", "Submission", "first", "get", False),
+        PTest("staff_get_same_course",
+              "staff", "Submission", "first", "get", True),
+        PTest("staff_get_other_course",
+              "staff", "Submission", "second", "get", False),
+        PTest("admin_get_student0",
+              "admin", "Submission", "first", "get", True),
+        PTest("admin_get_student1",
+              "admin", "Submission", "second", "get", True),
+        PTest("admin_delete_own_student",
+              "admin", "Submission", "first", "delete", False),
+        PTest("staff_delete_own_student",
+              "admin", "Submission", "first", "delete", False),
+        PTest("student_delete_submission",
+              "student0", "Submission", "first", "delete", False),
+        PTest("student_modify_submission",
+              "student0", "Submission", "first", "modify", False),
+        PTest("g",
+              "student0", "Assignment", "first", "get", True),
+        PTest("anon_get_assignment",
+              "anon", "Assignment", "first", "get", True),
+        PTest("student_create_assignment",
+              "student0", "Assignment", "first", "create", False),
+        PTest("admin_create_assignment",
+              "admin", "Assignment", "first", "create", True),
+        PTest("admin_delete_normal",
+              "admin", "Assignment", "first", "delete", False),
+        PTest("staff_delete_normal",
+              "staff", "Assignment", "first", "delete", False),
+        PTest("admin_delete_empty",
+              "admin", "Assignment", "empty", "delete", False),
+        PTest("staff_delete_empty",
+              "staff", "Assignment", "empty", "delete", False),
+        PTest("staff_delete_empty",
+              "empty_staff", "Assignment", "empty", "delete", False),
+        PTest("anon_get_course",
+              "anon", "Course", "first", "get", True),
+        PTest("student_get_course",
+              "student0", "Course", "first", "get", True),
+        PTest("admin_get_course",
+              "admin", "Course", "first", "get", True),
+        PTest("student_create_course",
+              "student0", "Course", "first", "create", False),
+        PTest("student_delete_course",
+              "student0", "Course", "first", "delete", False),
+        PTest("staff_create_course",
+              "staff", "Course", "first", "create", False),
+        PTest("staff_delete_course",
+              "staff", "Course", "first", "delete", False),
+        PTest("admin_create_course",
+              "admin", "Course", "first", "create", True),
+        PTest("admin_delete_course",
+              "admin", "Course", "first", "delete", True),
+        PTest("student_modify_course",
+              "student0", "Course", "first", "modify", False),
+        PTest("staff_modify_course",
+              "staff", "Course", "first", "modify", True),
+        PTest("a",
+              "student0", "User", "student0", "get", True),
+        PTest("b",
+              "student0", "User", "student1", "get", False),
+        PTest("staff_get_other_user",
+              "staff", "User", "student1", "get", False),
+        PTest("staff_get_same_user",
+              "staff", "User", "student0", "get", True),
+        PTest("admin_get_student1",
+              "admin", "User", "student1", "get", True),
+        PTest("admin_create_student1",
+              "admin", "User", "student1", "create", True),
+    ]
+
+    @data(*permission_tests)
+    def testAccess(self, value):
+        user_id, model, obj_name, need = value.input
+        self.login(user_id) # sets self.user
+
+        obj = None
+        if model == "User":
+            obj = self.accounts[obj_name]
         elif model == "Submission":
-            obj = self.submissions[args]
+            obj = self.submissions[obj_name]
         elif model == "Assignment":
-            obj = self.assignments[args]
+            obj = self.assignments[obj_name]
         elif model == "Course":
-            obj = self.courses[args]
-        else:
+            obj = self.courses[obj_name]
+
+        if not obj:
             self.assertTrue(False, "Invalid test arguments %s" % model)
 
-        self.assertEqual(result, obj.can(self.user, Need(need), obj),
-                value)
+        self.assertEqual(value.output, obj.can(self.user, Need(need), obj))
 
-   #  def testUserCanGetSelf(self):
-   #      self.login('dummy_student')
-   #      self.assertTrue(self.user.can(self.user, Need('get')))
-
-   #  def testUserCannotGetOther(self):
-   #      self.login('dummy_student')
-   #      other = self.accounts['other_student']
-
-   #      self.assertFalse(self.user.can(other, Need('get')))
-   #      permissions_tests.json
-   #  def test_get_non_admin(self):
-   #      """Tests that a get with a student access token works."""
-   #      self.get('/{}'.format(self.name))
-   #      self.assertStatusCode(200)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
