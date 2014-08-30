02acaeb10c8c043d76390f2f4dcc854536029685
Albert Wu <albert12132@gmail.com>
2014-08-29 21:39:38 -0700

    Add suites to demo_assignments

--- client/demo_assignments/valid/hw1/tests/q1.py
+++ client/demo_assignments/valid/hw1/tests/q1.py
@@ -10,48 +10,52 @@ test = {
     """,
   },
   'suites': [
-    {
-      'type': 'concept',
-      'question': """
-      What is the domain and range of the square function?
-      """,
-      'answer': """
-      Domain is numbers. Range is numbers
-      """,
-      'choices': [
-        'Domain is numbers. Range is strings',
-        'Domain is strings. Range is numbers',
-        'Domain is strings. Range is strings',
-      ],
-    },
-    {
-      'type': 'doctest',
-      'test': """
-      >>> square(3)
-      9
-      """,
-    },
-    {
-      'type': 'doctest',
-      'test': """
-      >>> square(-2)
-      4
-      # explanation: Squaring a negative number
-      # choice: -4
-      # choice: 2
-      # choice: None
-      """,
-    },
-    {
-      'type': 'doctest',
-      'test': """
-      >>> square(0)
-      0
-      # explanation: Squaring zero
-      """,
-      'teardown': """
-      print('Optional Teardown code for the testcase goes here')
-      """
-    },
+    [
+      {
+        'type': 'concept',
+        'question': """
+        What is the domain and range of the square function?
+        """,
+        'answer': """
+        Domain is numbers. Range is numbers
+        """,
+        'choices': [
+          'Domain is numbers. Range is strings',
+          'Domain is strings. Range is numbers',
+          'Domain is strings. Range is strings',
+        ],
+      },
+      {
+        'type': 'doctest',
+        'test': """
+        >>> square(3)
+        9
+        """,
+      },
+    ],
+    [
+      {
+        'type': 'doctest',
+        'test': """
+        >>> square(-2)
+        4
+        # explanation: Squaring a negative number
+        # choice: -4
+        # choice: 2
+        # choice: None
+        """,
+      },
+      {
+        'type': 'doctest',
+        'test': """
+        >>> square(0)
+        0
+        # explanation: Squaring zero
+        """,
+        'teardown': """
+        print('Optional Teardown code for the testcase goes here')
+        """
+      },
+    ],
   ],
 }
--- client/demo_assignments/valid/hw1/tests/q2.py
+++ client/demo_assignments/valid/hw1/tests/q2.py
@@ -10,48 +10,52 @@ test = {
     """,
   },
   'suites': [
-    {
-      'type': 'concept',
-      'question': """
-      What is the domain and range of the double function?
-      """,
-      'answer': """
-      Domain is numbers. Range is numbers
-      """,
-      'choices': [
-        'Domain is numbers. Range is strings',
-        'Domain is strings. Range is numbers',
-        'Domain is strings. Range is strings',
-      ],
-    },
-    {
-      'type': 'doctest',
-      'test': """
-      >>> double(3)
-      6
-      """,
-    },
-    {
-      'type': 'doctest',
-      'test': """
-      >>> double(-2)
-      -4
-      # explanation: Doubling a negative number
-      # choice: 4
-      # choice: 2
-      # choice: None
-      """,
-    },
-    {
-      'type': 'doctest',
-      'test': """
-      >>> double(0)
-      0
-      # explanation: Doubling zero
-      """,
-      'teardown': """
-      print('Optional Teardown code for the testcase goes here')
-      """
-    },
+    [
+      {
+        'type': 'concept',
+        'question': """
+        What is the domain and range of the double function?
+        """,
+        'answer': """
+        Domain is numbers. Range is numbers
+        """,
+        'choices': [
+          'Domain is numbers. Range is strings',
+          'Domain is strings. Range is numbers',
+          'Domain is strings. Range is strings',
+        ],
+      },
+    ],
+    [
+      {
+        'type': 'doctest',
+        'test': """
+        >>> double(3)
+        6
+        """,
+      },
+      {
+        'type': 'doctest',
+        'test': """
+        >>> double(-2)
+        -4
+        # explanation: Doubling a negative number
+        # choice: 4
+        # choice: 2
+        # choice: None
+        """,
+      },
+      {
+        'type': 'doctest',
+        'test': """
+        >>> double(0)
+        0
+        # explanation: Doubling zero
+        """,
+        'teardown': """
+        print('Optional Teardown code for the testcase goes here')
+        """
+      },
+    ],
   ]
 }
