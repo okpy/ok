0d3a1bc595df3fa2ac6011d55e12af225da5cb40
Albert Wu <albert12132@gmail.com>
2014-08-30 20:08:28 -0700

    Rewrite ConceptCase

--- client/models/concept_case.py
+++ client/models/concept_case.py
@@ -7,21 +7,25 @@ ConceptTestCases simply display the answer if already unlocked.
 """
 
 from models import core
+from models import serialize
 from protocols import grading
 from protocols import unlock
-import utils
+from utils import utils
 
-class ConceptTestCase(grading.GradedTestCase, unlock.UnlockTestCase):
+class ConceptCase(grading.GradedTestCase, unlock.UnlockTestCase):
     """TestCase for conceptual questions."""
 
     type = 'concept'
 
-    @property
-    def answer(self):
-        """Returns the answer of the test case. If the test case has
-        not been unlocked, the answer will remain in locked form.
-        """
-        return self._outputs[0].answer
+    REQUIRED = {
+        'type': serialize.STR,
+        'question': serialize.STR,
+        'answer': serialize.STR,
+    }
+    OPTIONAL = {
+        'locked': serialize.BOOL_TRUE,
+        'choices': serialize.SerializeArray(serialize.STR),
+    }
 
     ######################################
     # Protocol interface implementations #
@@ -31,54 +35,17 @@ class ConceptTestCase(grading.GradedTestCase, unlock.UnlockTestCase):
         """Implements the GradedTestCase interface."""
         if verbose:
             utils.underline('Concept question', line='-')
-            print(self._input_str)
-            print('A: ' + self.answer)
+            print(self['question'])
+            print('A: ' + self['answer'])
             print()
         return False
 
+    def should_grade(self):
+        return not self['locked']
+
     def on_unlock(self, logger, interact_fn):
         """Implements the UnlockTestCase interface."""
-        print(self._input_str)
-        hash_key = self.info['hash_key'].encode('utf-8')
-        verify_fn = lambda x, y: hmac.new(hash_key, x.encode('utf-8')).digest() == y
-        answer = interact_fn(self.answer, self.verify_fn)
-        return [core.TestCaseAnswer(answer)]
-
-    #################
-    # Serialization #
-    #################
-
-    @classmethod
-    def deserialize(cls, case_json, assignment_info):
-        """Deserializes a JSON object into a Test object, given a
-        particular set of assignment_info.
-
-        PARAMETERS:
-        case_json       -- JSON; the JSON representation of the case.
-        assignment_info -- JSON; information about the assignment,
-                           may be used by TestCases.
-
-        RETURNS:
-        Test
-        """
-        output = core.TestCaseAnswer(case_json['answer'],
-                                     case_json.get('choices', None))
-        return cls(case_json['question'], [output],
-                   **case_json['status'])
-
-    def serialize(self):
-        """Serializes this Test object into JSON format.
-
-        RETURNS:
-        JSON as a plain-old-Python-object.
-        """
-        json = {
-            'type': self.type,
-            'question': self._input_str,
-            'answer': self.answer,
-            'status': self._status
-        }
-        if self.outputs[0].is_multiple_choice:
-            json['choices'] = self._outputs[0].choices
-        return json
+        print(self['question'])
+        answer = interact_fn(self['answer'], self['choices'])
+        self['answer'] = answer
 
