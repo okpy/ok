e8641f0e6335ea61d9697f8e2e766bc5503cfda1
Soumya Basu <soumyab@berkeley.edu>
2014-09-01 17:35:02 -0700

    Change config.py to the correct test name to fix publishing

--- client/config.py
+++ client/config.py
@@ -2,8 +2,8 @@ import models.concept_case
 import models.doctest_case
 
 cases = { 
-          'concept' : models.concept_case.ConceptTestCase,
-          'python' : models.doctest_case.PythonTestCase
+          'concept' : models.concept_case.ConceptCase,
+          'python' : models.doctest_case.DoctestCase
         }
 
 protocols = [ 'grading', 'unlock' ]
