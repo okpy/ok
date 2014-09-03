80061524e95e8751849fc246774f47e5d5b5c34e
Stephen Martinis <moowiz2020@gmail.com>
2014-09-03 15:36:16 -0700

    Changed upload_version.py

--- client/upload_version.py
+++ client/upload_version.py
@@ -1,18 +1,27 @@
+"""
+Uploads the current version of ok.py to the server.
+"""
+
 import requests
 import base64
 
-with open('ok', 'rb') as okzip:
-    file_data = base64.b64encode(okenc.read()).decode('utf-8')
+def main():
+    with open('ok', 'rb') as okzip:
+        file_data = base64.b64encode(okzip.read()).decode('utf-8')
 
-params = {
-    'access_token': "ya29.dQCiSYYqeLchvyEAAACgsU7OUS9sPftIYTJzJ5MsR6XZm2QH8HO4YjDBqaGGT1gojmbG6PGMEkSa7jx3yG8"
-}
+    params = {
+        'access_token': raw_input("Please enter your access token:")
+    }
 
-data = {
-    'name': 'okpy',
-    'version': '1.1.0',
-    'file_data': file_data
-}
+    data = {
+        'name': 'okpy',
+        'version': '1.1.0',
+        'file_data': file_data
+    }
 
-r = requests.post('https://ok-server.appspot.com/api/v1/version', params=params, data=data)
-print r
+    resp = requests.post('https://ok-server.appspot.com/api/v1/version',
+            params=params, data=data)
+    print resp
+
+if __name__ == "__main__":
+    main()
