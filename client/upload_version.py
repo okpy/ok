81a710a520ab2122dda9aed4ddd23977d0c6b10d
Stephen Martinis <moowiz2020@gmail.com>
2014-09-03 15:41:58 -0700

    Made upload version do a put.

--- client/upload_version.py
+++ client/upload_version.py
@@ -5,6 +5,12 @@ Uploads the current version of ok.py to the server.
 import requests
 import base64
 
+SERVER_URL = 'https://ok-server.appspot.com/api/v1/version'
+
+def get_current_id():
+    resp = requests.get(SERVER_URL + "?name=okpy")
+    return resp.json()['data']['results'][0]['id']
+
 def main():
     with open('ok', 'rb') as okzip:
         file_data = base64.b64encode(okzip.read()).decode('utf-8')
@@ -14,13 +20,11 @@ def main():
     }
 
     data = {
-        'name': 'okpy',
-        'version': '1.1.0',
+        'version': '1.1.1',
         'file_data': file_data
     }
 
-    resp = requests.post('https://ok-server.appspot.com/api/v1/version',
-            params=params, data=data)
+    resp = requests.put(SERVER_URL + '/%s' % get_current_id(), params=params, data=data)
     print resp
 
 if __name__ == "__main__":
