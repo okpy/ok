80108f37c74341ee96fca73bbf12b8ae338fcd18
Stephen Martinis <moowiz2020@gmail.com>
2014-09-04 13:38:11 -0700

    Added api fields tests.

---
+++ server/tests/test_api_fields.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#pylint: disable=no-member, no-init, too-many-public-methods
+#pylint: disable=attribute-defined-outside-init
+# This disable is because the tests need to be name such that
+# you can understand what the test is doing from the method name.
+#pylint: disable=missing-docstring
+"""
+Tests api field filtering.
+"""
+from app.utils import coerce_to_json
+from test_base import BaseTestCase, unittest
+
+class FakeModel(object):
+    # Disabling because it's just a test object
+    #pylint: disable=too-few-public-methods
+    """
+    Test class that fakes out to_json
+    """
+
+    num = 0
+    def __init__(self):
+        self.num = FakeModel.num
+        FakeModel.num += 1
+
+    def to_json(self, fields=None):
+        return 'json' + str(self.num)
+
+class FieldsTest(BaseTestCase):
+    """
+    Tests for api fields feature.
+    """
+    def setUp(self):
+        super(FieldsTest, self).setUp()
+        FakeModel.num = 0
+
+    def coerce(self, data, fields=None):
+        if not fields:
+            fields = {}
+        self.data = coerce_to_json(data, fields)
+
+    def test_basic_all(self):
+        """
+        Tests coercing a basic value.
+        """
+        self.coerce(FakeModel())
+        self.assertEquals('json0', self.data)
+
+    def test_list_all(self):
+        """
+        Tests coercing a list.
+        """
+        self.coerce([FakeModel(), FakeModel()])
+        self.assertEquals(['json0', 'json1'], self.data)
+
+    def test_dict_all(self):
+        """
+        Tests coercing a dictionary.
+        """
+        self.coerce({
+            'a': FakeModel(),
+            'b': FakeModel()
+        })
+        self.assertEquals({
+            'a': 'json0',
+            'b': 'json1'
+        }, self.data)
+
+    def test_nested_all(self):
+        """
+        Tests coercing a nested structure.
+        """
+        self.coerce({
+            'a': {
+                'd': FakeModel(),
+                'e': FakeModel()
+            },
+            'b': FakeModel()
+        })
+        self.assertEquals({
+            'a': {
+                'd': 'json0',
+                'e': 'json1',
+            },
+            'b': 'json2',
+        }, self.data)
+
+    def test_nested_fields(self):
+        """
+        Tests coercing a nested structure with filtering fields.
+        """
+        test_self = self
+        class TestModel(object):
+            # Disabling because it's just a test object
+            #pylint: disable=too-few-public-methods
+            def to_json(self, fields):
+                test_self.assertEqual(fields, ['foo'])
+
+        self.coerce({
+            'a': {
+                'b': TestModel(),
+            }
+        }, {'a': {'b': ['foo']}})
+
+
+if __name__ == '__main__':
+    unittest.main()
+
