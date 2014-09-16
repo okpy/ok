d3a151f0c3f8fc0640e8c6f69c44281db1a0d88c
Sumukh Sridhara <sumukh1@gmail.com>
2014-09-16 16:22:45 -0700

    basic versions,ssl graceful errors

--- client/__main__.py
+++ client/__main__.py
@@ -1,5 +1,18 @@
-import ok
 import sys
 sys.path.append('.')
 
+# List of unsupported in (Major, Minor, Micro) form. 
+# unsupported_versions = [(1,2,2)]
+
+# Basic Version Checking
+if sys.version_info[0] < 3:
+	sys.exit("ok requires Python 3. \nFor more info: http://www-inst.eecs.berkeley.edu/~cs61a/fa14/lab/lab01/#installing-python")
+
+try: 
+	import ssl
+except Exception as e:
+	print(e)
+	sys.exit("Please run ok with the --local flag \n i.e. python3 ok -u --local")
+
+import ok
 ok.ok_main(ok.parse_input())
