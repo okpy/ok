1566eaef7b9ca947ff957a83015cfbde3e5ce7bd
Sharad Vikram <sharad.vikram@gmail.com>
2014-09-15 21:02:58 -0700

    max check

--- server/app/api.py
+++ server/app/api.py
@@ -418,10 +418,12 @@ class GroupAPI(APIResource):
     def add_member(self, obj, user):
         data = self.parse_args(False, user)
         group_obj = self.model.get_by_id(obj.key.id())
+        assignment = group_obj.assignment.get()
         for member in data['members']:
             if member not in group_obj.members:
                 group_obj.members.append(member)
-        print group_obj.members
+            if len(group_obj.members) > assignment.max_group_size:
+                return {}
         group_obj.put()
 
     def remove_member(self, obj, user):
