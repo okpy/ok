78e165dd1259fd88a1775cd59790453683203628
Sharad Vikram <sharad.vikram@gmail.com>
2014-09-29 02:51:21 -0700

    made seed better

--- server/app/seed/__init__.py
+++ server/app/seed/__init__.py
@@ -15,7 +15,8 @@ def seed():
             creator=creator.key,
             staff=[])
 
-    def make_fake_assignment(course, creator):
+    def make_future_assignment(course, creator):
+        date = (datetime.datetime.now() + datetime.timedelta(days=365))
         with open('app/seed/hog_template.py') as fp:
             templates = {}
             templates['hog.py'] = fp.read()
@@ -27,7 +28,22 @@ def seed():
             course=course.key,
             creator=creator.key,
             max_group_size=4,
-            due_date=datetime.datetime.now())
+            due_date=date)
+
+    def make_past_assignment(course, creator):
+        date = (datetime.datetime.now() - datetime.timedelta(days=365))
+        with open('app/seed/hog_template.py') as fp:
+            templates = {}
+            templates['hog.py'] = fp.read()
+        return models.Assignment(
+            name='proj2',
+            points=20,
+            display_name="Trends",
+            templates=json.dumps(templates),
+            course=course.key,
+            creator=creator.key,
+            max_group_size=4,
+            due_date=date)
 
     def make_fake_submission(assignment, submitter):
         with open('app/seed/hog_modified.py') as fp:
@@ -51,8 +67,10 @@ def seed():
     c.put()
     course = make_fake_course(c)
     course.put()
-    assign = make_fake_assignment(course, c)
+    assign = make_future_assignment(course, c)
     assign.put()
+    assign2 = make_past_assignment(course, c)
+    assign2.put()
     subm = make_fake_submission(assign, c)
     subm.put()
 
