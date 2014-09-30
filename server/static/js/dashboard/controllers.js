6a8be71f43d319fc585db6e54160fba62b49f541
Sharad Vikram <sharad.vikram@gmail.com>
2014-09-29 21:37:35 -0700

    front end hotfix

--- server/static/js/dashboard/controllers.js
+++ server/static/js/dashboard/controllers.js
@@ -8,9 +8,35 @@ app.controller("AssignmentModuleController", ["$scope", "Assignment",
     }
   ]);
 
+
+app.controller("SubmissionDashboardController", ["$scope", "Submission",
+    function ($scope, Submission) {
+      Submission.query({
+        fields: {
+          'created': true,
+          'id': true,
+          'submitter': {
+            'id': true
+          },
+          'assignment': {
+            'name': true,
+            'display_name': true,
+            'id': true,
+          },
+        },
+        page: 1,
+        num_page: 2,
+      }, function(response) {
+        $scope.submissions = response.data.results;
+      });
+    }
+  ]);
+
 app.controller("SubmissionModuleController", ["$scope", "Submission",
     function ($scope, Submission) {
-      Submission.query(function(response) {
+      Submission.query({
+        fields: false,
+      }, function(response) {
         $scope.submissions = response.data.results;
       });
     }
