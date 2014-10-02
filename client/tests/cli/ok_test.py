3552bda0124180b404dd9ea0577f1f77cd2b4037
Albert Wu <albert12132@gmail.com>
2014-10-02 10:48:54 -0700

    Temporary fix for ok_test

--- client/tests/cli/ok_test.py
+++ client/tests/cli/ok_test.py
@@ -2,11 +2,12 @@ from client.cli import ok
 import sys
 import unittest
 
+# TODO(soumya): This should be rewritten as an integration test
 class ArgparseTest(unittest.TestCase):
     def test_parse_input(self):
         old_sys_argv = sys.argv[1:]
         sys.argv[1:] = []
-        _ = ok.parse_input() # Does not crash and returns a value.
+        _ = ok.parse_input([]) # Does not crash and returns a value.
         sys.argv[1:] = old_sys_argv
 
 
