fb2f75f3f6217e6153e5777889d530562989b57d
John DeNero <papajohn@gmail.com>
2015-01-21 22:43:20 -0800

    add back group API methods

--- server/app/models.py
+++ server/app/models.py
@@ -156,16 +156,6 @@ class User(Base):
 
         return all_backups
 
-    def get_submissions(self, assignment):
-        all_submissions = []
-
-        for backup in self.get_backups(assignment):
-            if len(Submission.query(Submission.backup == backup.key)) > 0:
-                all_submissions.append(backup)
-
-        return all_submissions
-
-
     def get_group(self, assignment):
         query = Group.query(Group.member == self.key)
         group = query.filter(Group.assignment == assignment)
@@ -178,7 +168,6 @@ class User(Base):
         for assignment in course.assignments():
             assign_info['group'] = self.get_group(assignment)
             assign_info['final'] = self.get_final_submission(assignment)
-            assign_info['backups'] = self.get_backups(assignment)
             assign_info['assignment'] = assignment
             info['assignments'].append(assign_info)
 
@@ -662,6 +651,8 @@ class Group(Base):
     #@ndb.transactional
     def accept(self, user_key):
         """User accepts an invitation to join. Returns error or None."""
+        if isinstance(user_key, User):
+            user_key = user_key.key
         if user_key not in self.invited:
             return "That user is not invited to the group"
         if user_key in self.member:
@@ -673,6 +664,8 @@ class Group(Base):
     #@ndb.transactional
     def exit(self, user_key):
         """User leaves the group. Empty/singleton groups are deleted."""
+        if isinstance(user_key, User):
+            user_key = user_key.key
         for users in [self.member, self.invited]:
             if user_key in users:
                 users.remove(user_key)
