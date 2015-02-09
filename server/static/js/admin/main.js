4db5f39b482ff5af568aed07d2733905c71a21e2
Sumukh Sridhara <sumukh1@gmail.com>
2015-02-09 05:22:52 -0800

    Add Diffs to Staff view, get comments saved

---
+++ server/static/js/admin/main.js
@@ -0,0 +1,39 @@
+app.directive('snippet', function() {
+        "use strict";
+        return {
+            restrict: 'E',
+            templateUrl: '/static/partials/common/snippet.html',
+            link: function(scope, elem, attrs) {
+                scope.contents = scope.contents.split('\n');
+            }
+        };
+    });
+
+app.directive('diff', function() {
+        "use strict";
+        return {
+            restrict: 'E',
+            templateUrl: '/static/partials/admin/diff.html',
+        };
+    });
+
+app.directive('group', function() {
+        "use strict";
+        return {
+            restrict: 'E',
+            templateUrl: '/static/partials/common/group.html',
+        };
+    });
+
+app.directive('comments', function() {
+        "use strict";
+        return {
+            scope: false,
+            restrict: 'E',
+            templateUrl: '/static/partials/common/comment-viewer.html',
+        };
+    });
+
+app.directive('datatableSetup', function () {
+    return { link: function (scope, elm, attrs) { console.log(Sortable.init());  } }
+});
