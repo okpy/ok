e66bb8bb415395a8d1a60c5761484012eaebf85b
Sumukh Sridhara <sumukh1@gmail.com>
2015-02-18 00:46:13 -0800

    fix off by one error in commenting, fixes #373

--- server/static/js/admin/controllers.js
+++ server/static/js/admin/controllers.js
@@ -555,7 +555,7 @@ app.controller("WriteCommentController", ["$scope", "$sce", "$stateParams", "Sub
         Submission.addComment({
           id: $scope.backupId,
           file: $scope.file_name,
-          index: $scope.codeline.rightNum,
+          index: $scope.codeline.rightNum - 1,
           message: text,
         }, function (resp) {
           resp.self = true
