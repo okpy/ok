f30cf94597343a08ef0f7185bd6bddda9e2f353a
Fred Hong <fredhong@berkeley.edu>
2015-04-02 19:42:03 -0700

    removed unused taskqueue

--- server/tests/unittests/test_autograder.py
+++ server/tests/unittests/test_autograder.py
@@ -123,7 +123,6 @@ class AutograderTests(BaseTestCase):
 
     def test_addToQueue_and_lease(self):
         add_taskqueue(self.course, self.assignment.key)
-        q = taskqueue.Queue("pull-queue")
         submissions = lease_tasks()
         self.assertEquals(len(submissions), 3)             
 
