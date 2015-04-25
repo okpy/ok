03c6e4c48c3ece36f69f678237b8c4db4ab8f7fb
Alvin Wan <hi@alvinwan.com>
2015-04-24 22:27:16 -0700

    linked blob with sidebar

--- server/static/student/scripts/course.js
+++ server/static/student/scripts/course.js
@@ -1,18 +1,28 @@
 $(document).ready(function() {
+
     $('.flip').on('click',function() {
         $('.blob-more').removeClass('flipped');
     });
     $('.blob-action').on('click',function() {
+        console.log("GOTCHA")
 //        $('.flip').click();
 //        $(this).parent().parent().children('.blob-more').addClass('flipped');
-        $('.container-fluid').addClass('active');
-        var blob = $(this).parent();
-        $('.sidebar').attr('color', blob.attr('color'));
+        openDetails($(this));
     });
     $('.sidebars .close').on('click',function() {
-        $('.container-fluid').removeClass('active');
+        closeDetails();
     });
     $('.cover').on('click',function() {
         $('.sidebars .close').click();
     });
 });
+
+function openDetails(element) {
+    $('.wrap-container').addClass('active');
+    var blob = element.parent();
+    $('.sidebar').attr('color', blob.attr('color'));
+}
+
+function closeDetails() {
+    $('.wrap-container').removeClass('active');
+}
