c9c7b8d11517e158b8b7465ebb6cc40da73f19f8
LuiseV <valentin.luise@gmail.com>
2015-06-23 23:13:22 -0400

    added download CSV to AssignmentDetailCtrl and specified URL for API call

--- server/static/js/admin/controllers.js
+++ server/static/js/admin/controllers.js
@@ -49,7 +49,10 @@ app.controller("AssignmentListCtrl", ['$scope', '$http', 'Assignment',
 app.controller("AssignmentDetailCtrl", ["$scope", "$stateParams", "Assignment",
   function ($scope, $stateParams, Assignment) {
     $scope.assignment = Assignment.get({id: $stateParams.assignmentId});
-  }
+
+    $scope.download_compScores = function() {
+      Assignment.download_composition_scores({});
+    }
   ]);
 
 app.controller("AssignmentCreateCtrl", ["$scope", "$window", "$state", "$stateParams", "Assignment", "Course",
