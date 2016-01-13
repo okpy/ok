d8b3f2ffb265897db557725d345e8a49eb4a51c2
Kyle Raftogianis <knrafto@gmail.com>
2016-01-12 20:06:20 -0800

    Test users

---
+++ tests/test_user.py
@@ -0,0 +1,15 @@
+from helpers import OkTestCase
+
+from server.models import User
+
+class TestUser(OkTestCase):
+    def test_from_email(self):
+        email = 'martymcfly@aol.com'
+
+        user1 = User.from_email(email)
+        assert user1.email == email
+        assert not user1.is_admin
+        assert user1.active
+
+        user2 = User.from_email(email)
+        assert user1.id == user2.id
