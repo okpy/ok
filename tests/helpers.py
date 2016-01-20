831f080177495527b579f38e1a6d2957b7b9c280
Kyle Raftogianis <knrafto@gmail.com>
2016-01-20 14:01:17 -0800

    Add trailing slash to auth routes

--- tests/helpers.py
+++ tests/helpers.py
@@ -16,7 +16,7 @@ class OkTestCase(TestCase):
 
     def login(self, email):
         """ Log in as an email address """
-        response = self.client.post('/testing-login/authorized', data={
+        response = self.client.post('/testing-login/authorized/', data={
             'email': email
         }, follow_redirects=True)
         self.assert_200(response)
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -4,21 +4,21 @@ class TestAuth(OkTestCase):
     email = 'martymcfly@aol.com'
 
     def test_login(self):
-        """GET /login should redirect to Google OAuth."""
-        response = self.client.get('/login')
+        """GET /login/ should redirect to Google OAuth."""
+        response = self.client.get('/login/')
         assert response.location.startswith('https://accounts.google.com/o/oauth2/auth')
 
     def test_testing_login(self):
-        """GET /testing-login should show a test login page."""
-        response = self.client.get('/testing-login')
+        """GET /testing-login/ should show a test login page."""
+        response = self.client.get('/testing-login/')
         self.assert_200(response)
         self.assert_template_used('testing-login.html')
 
     def test_testing_login_fail(self):
-        """GET /testing-login should 404 if TESTING_LOGIN config is not set."""
+        """GET /testing-login/ should 404 if TESTING_LOGIN config is not set."""
         app = self.create_app()
         app.config['TESTING_LOGIN'] = False
-        response = app.test_client().get('/testing-login')
+        response = app.test_client().get('/testing-login/')
         self.assert_404(response)
 
     def test_restricted(self):
@@ -27,6 +27,6 @@ class TestAuth(OkTestCase):
         response = self.client.get('/student/')
         self.assert_200(response)
 
-        self.client.get('/logout')
+        self.client.get('/logout/')
         response = self.client.get('/student/')
-        self.assert_redirects(response, '/login')
+        self.assert_redirects(response, '/login/')
