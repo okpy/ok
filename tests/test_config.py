d9a93c53a5aaab9f2ca9ce7d879efb94ed92615e
Sumukh Sridhara <sumukh1@gmail.com>
2016-01-24 21:38:03 -0800

    fix travis config/tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,7 +8,7 @@ class TestConfig:
         """ Tests if the development config loads correctly """
 
         app = create_app('server.settings.dev.DevConfig')
-        dev_db = 'sqlite:///../database.db'
+        dev_db = 'sqlite:///../development.db'
 
         assert app.config['DEBUG'] is True
         assert app.config['SQLALCHEMY_DATABASE_URI'] == dev_db
@@ -18,7 +18,7 @@ class TestConfig:
         """ Tests if the test config loads correctly """
 
         app = create_app('server.settings.test.TestConfig')
-        test_db = 'sqlite:///../database.db'
+        test_db = 'sqlite:///../testing.db'
 
         assert app.config['DEBUG'] is True
         assert app.config['SQLALCHEMY_DATABASE_URI'] == test_db
