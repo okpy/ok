eb89aff58103aa08bab592ebbc38171098f37c82
Kyle Raftogianis <knrafto@gmail.com>
2016-01-31 14:12:17 -0800

    Render unified diffs

--- server/highlight.py
+++ server/highlight.py
@@ -13,26 +13,38 @@ def highlight(filename, source):
 def highlight_diff(filename, a, b):
     """Given two input strings, generate a sequence of 4-tuples. The elements
     of each tuple are:
-        * 'delete', 'insert', or 'equal' (the tag)
+        * 'delete', 'insert', 'equal', or 'header' (the tag)
         * the line number of the first file (or None)
         * the line number of the second file (or None)
         * the rendered HTML string of the line
     """
     highlighted_a = highlight(filename, a).splitlines()
     highlighted_b = highlight(filename, b).splitlines()
+
+    def delete(i1, i2):
+        for i in range(i1, i2):
+            yield 'delete', i + 1, None, '-' + highlighted_a[i]
+
+    def insert(i1, i2):
+        for j in range(j1, j2):
+            yield 'insert', None, j + 1, '+' + highlighted_b[j]
+
+    def equal(i1, i2, j1, j2):
+        for i, j in zip(range(i1, i2), range(j1, j2)):
+            yield 'equal', i + 1, j + 1, ' ' + highlighted_b[j]
+
     matcher = difflib.SequenceMatcher(None, a.splitlines(), b.splitlines())
-    for tag, i1, i2, j1, j2 in matcher.get_opcodes():
-        if tag == 'replace':
-            for i in range(i1, i2):
-                yield 'delete', i + 1, None, highlighted_a[i]
-            for j in range(j1, j2):
-                yield 'insert', None, j + 1, highlighted_b[j]
-        elif tag == 'delete':
-            for i in range(i1, i2):
-                yield 'delete', i + 1, None, highlighted_a[i]
-        elif tag == 'insert':
-            for j in range(j1, j2):
-                yield 'insert', None, j + 1, highlighted_b[j]
-        elif tag == 'equal':
-            for i, j in zip(range(i1, i2), range(j1, j2)):
-                yield 'equal', i + 1, j + 1, highlighted_b[j]
+    for group in matcher.get_grouped_opcodes():
+        first, last = group[0], group[-1]
+        header = '@@ -{},{} +{},{} @@'.format(first[1], last[2], first[3], last[4])
+        yield 'header', None, None, header
+        for tag, i1, i2, j1, j2 in group:
+            if tag == 'replace':
+                yield from delete(i1, i2)
+                yield from insert(j1, j2)
+            elif tag == 'delete':
+                yield from delete(i1, i2)
+            elif tag == 'insert':
+                yield from insert(j1, j2)
+            elif tag == 'equal':
+                yield from equal(i1, i2, j1, j2)
