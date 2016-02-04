444d8268a93dded4e7d630f78d90d7dc0e69d73f
Kyle Raftogianis <knrafto@gmail.com>
2016-02-03 18:36:49 -0800

    Move time formatting to utils.py

--- server/utils.py
+++ server/utils.py
@@ -36,3 +36,7 @@ class HashidConverter(BaseConverter):
 
     def to_url(self, value):
         return encode_id(value)
+
+def time(dt, course):
+    """Format a time string in a course's locale."""
+    return course.timezone.localize(dt).strftime('%a %m/%d %H:%M %p')
