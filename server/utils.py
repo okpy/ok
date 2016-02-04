9477b472b6b00799dee7fecb40aafdfcf0036ef3
Kyle Raftogianis <knrafto@gmail.com>
2016-02-04 15:55:21 -0800

    Reindent is_safe_redirect_url

--- server/utils.py
+++ server/utils.py
@@ -20,7 +20,7 @@ def local_time(dt, course):
     return course.timezone.localize(dt).strftime('%a %m/%d %H:%M %p')
 
 def is_safe_redirect_url(request, target):
-  host_url = urlparse(request.host_url)
-  redirect_url = urlparse(urljoin(request.host_url, target))
-  return redirect_url.scheme in ('http', 'https') and \
-    host_url.netloc == redirect_url.netloc
+    host_url = urlparse(request.host_url)
+    redirect_url = urlparse(urljoin(request.host_url, target))
+    return redirect_url.scheme in ('http', 'https') and \
+        host_url.netloc == redirect_url.netloc
