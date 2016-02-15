170a1aaec1cbd2ad9ea0634f85f40dc84bc363e9
Kyle Raftogianis <knrafto@gmail.com>
2016-02-15 15:16:56 -0800

    Allow submissions up to and including lock date

--- server/models.py
+++ server/models.py
@@ -169,7 +169,7 @@ class Assignment(Model):
 
     @hybrid_property
     def active(self):
-        return dt.utcnow() < self.lock_date  # TODO : Ensure all times are UTC
+        return dt.utcnow() <= self.lock_date
 
     @staticmethod
     def by_name(name):
