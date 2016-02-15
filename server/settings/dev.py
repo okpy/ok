1bbcf4632d14249c2c424e3e09ee6cccdfa48e97
Kyle Raftogianis <knrafto@gmail.com>
2016-02-15 13:39:27 -0800

    Use mysql

--- server/settings/dev.py
+++ server/settings/dev.py
@@ -3,7 +3,7 @@ from server.settings import LocalConfig
 class DevConfig(LocalConfig):
     ENV = 'dev'
     DEBUG_TB_INTERCEPT_REDIRECTS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///../development.db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://okdev@localhost/okdev'
     RQ_LOW_URL = "redis://localhost:6379/1"
 
     CACHE_REDIS_URL = "redis://localhost:6379/0"
--- tests/test_config.py
+++ tests/test_config.py
@@ -8,18 +8,14 @@ class TestConfig:
         """ Tests if the development config loads correctly """
 
         app = create_app('server.settings.dev.DevConfig')
-        dev_db = 'sqlite:///../development.db'
 
         assert app.config['DEBUG'] is True
-        assert app.config['SQLALCHEMY_DATABASE_URI'] == dev_db
         assert app.config['CACHE_TYPE'] == 'simple'
 
     def test_test_config(self):
         """ Tests if the test config loads correctly """
 
         app = create_app('server.settings.test.TestConfig')
-        test_db = 'sqlite:///../testing.db'
 
         assert app.config['DEBUG'] is True
-        assert app.config['SQLALCHEMY_DATABASE_URI'] == test_db
         assert app.config['CACHE_TYPE'] == 'null'
