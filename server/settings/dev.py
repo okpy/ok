c3561a084096dce6307f6d44cee1750d38401d63
Kyle Raftogianis <knrafto@gmail.com>
2016-02-16 21:54:21 -0800

    Simplify settings

--- server/settings/dev.py
+++ server/settings/dev.py
@@ -2,11 +2,3 @@ from server.settings import LocalConfig
 
 class DevConfig(LocalConfig):
     ENV = 'dev'
-    DEBUG_TB_INTERCEPT_REDIRECTS = False
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://okdev@localhost/okdev'
-    RQ_LOW_URL = "redis://localhost:6379/1"
-
-    CACHE_REDIS_URL = "redis://localhost:6379/0"
-    CACHE_TYPE = 'simple'
-
-    ASSETS_DEBUG = True
