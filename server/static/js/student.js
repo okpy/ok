4aaa718f37b771c35c1610b26bbb7974b7e5fd50
Kyle Raftogianis <knrafto@gmail.com>
2016-03-27 18:46:03 -0700

    Send CSRF token in AJAX requests

--- server/static/js/student.js
+++ server/static/js/student.js
@@ -1,4 +1,15 @@
 jQuery(document).ready(function($){
+    "use strict";
+
+    /* CSRF protection. The csrf_token variable is set in the
+     * 'student/base.html' template.
+     */
+    $(document).ajaxSend(function(e, xhr, s) {
+        if (s.type == 'POST' || s.type == 'PUT') {
+            xhr.setRequestHeader('X-CSRF-Token', csrf_token);
+        }
+    });
+
     $('.remove-confirm').on('click',function(e){
         e.preventDefault();
         var form = $(this).parents('form');
