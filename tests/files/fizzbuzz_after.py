0f0c5af3c1cb3379077133f8a94c734078b25f87
Sumukh Sridhara <sumukh1@gmail.com>
2016-06-17 03:03:40 -0700

    add emoji to file to ensure messages can handle utf8

--- tests/files/fizzbuzz_after.py
+++ tests/files/fizzbuzz_after.py
@@ -2,7 +2,7 @@ def fizz_buzz(n):
     for num in range(1, n + 1):
         msg = ''
         if num % 3 == 0:
-            msg += 'Fizz'
+            msg += 'Fizz🍪'
         if num % 5 == 0:
             msg += 'Buzz'
         print(msg or num)
