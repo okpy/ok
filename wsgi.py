1bd3743ea1077ccf769ac7795b3e0b6393c46921
Sumukh Sridhara <sumukh1@gmail.com>
2016-06-20 05:16:38 -0700

    update WSGI script to use ProxyFix

--- wsgi.py
+++ wsgi.py
@@ -3,8 +3,11 @@
 # gunicorn -b 0.0.0.0:5000 wsgi:app
 import os
 
-from server import create_app, generate
-from server.models import db, User
+from werkzeug.contrib.fixers import ProxyFix
+
+from server import create_app
 
 env = os.environ.get('OK_ENV', 'dev')
 app = create_app('settings/{0!s}.py'.format(env))
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
