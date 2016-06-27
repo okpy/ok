5289cb0c0c072189a741753bad479864b4e14c9d
Sumukh Sridhara <sumukh1@gmail.com>
2016-06-27 02:46:08 -0700

    more tests for scoring

--- tests/test_api.py
+++ tests/test_api.py
@@ -175,3 +175,28 @@ class TestAuth(OkTestCase):
         assert response.json['code'] == 401
 
 
+    def test_score_staff(self):
+        self._test_backup(True)
+
+        user = User.lookup(self.user1.email)
+        self.login(self.staff1.email)
+
+        response = self.client.post('/api/v3/score/')
+        self.assert_400(response)
+        assert response.json['code'] == 400
+        backup = Backup.query.filter(Backup.submitter_id == user.id).first()
+
+        data = {'bid': encode_id(backup.id), 'kind': 'Total',
+                'score': 128.2, 'message': 'wow'}
+        response = self.client.post('/api/v3/score/', data=data)
+        self.assert_200(response)
+        assert response.json['code'] == 200
+
+        self.logout()
+        self.login(self.admin.email)
+
+        data = {'bid': encode_id(backup.id), 'kind': 'Total',
+                'score': 128.2, 'message': 'wow'}
+        response = self.client.post('/api/v3/score/', data=data)
+        self.assert_200(response)
+        assert response.json['code'] == 200
