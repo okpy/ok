4bb4957e58c0fccaadef151d8e927d10cee27992
Sumukh Sridhara <sumukh1@gmail.com>
2016-07-01 17:54:08 -0700

    hide other courses by default, add button to view

--- server/static/js/student.js
+++ server/static/js/student.js
@@ -2,5 +2,5 @@ function showElem(selector) {
     if(!$(selector).is(':visible')) {
         $(selector).show();
     }
-    $("html, body").animate({ scrollTop: $(selector).offset().top }, 500);
+	$("html, body").animate({ scrollTop: $(selector).offset().top }, 500);
 }
