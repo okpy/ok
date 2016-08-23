d1f4958a245b60713a98e9c981b2f924631db772
kyletliang <kyletliang@gmail.com>
2016-08-23 15:42:33 -0700

    encoding specified for both original and modified file

--- server/generate.py
+++ server/generate.py
@@ -14,7 +14,7 @@ from server.models import (db, User, Course, Assignment, Enrollment, Group,
 from server.constants import VALID_ROLES, STUDENT_ROLE, TIMEZONE
 from server.extensions import cache
 
-original_file = open('tests/files/fizzbuzz_before.py').read()
+original_file = open('tests/files/fizzbuzz_before.py', encoding="utf8").read()
 modified_file = open('tests/files/fizzbuzz_after.py', encoding="utf8").read()
 
 def weighted_choice(choices):
