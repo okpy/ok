2f6770905bea578b1aeed12448802e33c07b3ac0
Sumukh Sridhara <sumukh1@gmail.com>
2016-08-28 17:38:59 -0700

    course.id fix failure

--- tests/test_enrollment.py
+++ tests/test_enrollment.py
@@ -3,9 +3,9 @@ from tests import OkTestCase
 from server.models import db, Enrollment, User
 from server.forms import EnrollmentForm, BatchEnrollmentForm
 from server.constants import STUDENT_ROLE, LAB_ASSISTANT_ROLE
-    
+
 class TestEnrollment(OkTestCase):
-    
+
     def setUp(self):
         super().setUp()
         self.studentA = {
@@ -46,80 +46,80 @@ class TestEnrollment(OkTestCase):
             'section': '102',
             'role': LAB_ASSISTANT_ROLE
         }
-    
+
     def test_create(self):
         self.setup_course()
-        
+
         user = User(name=self.studentA['name'], email=self.studentA['email'])
         db.session.add(user)
         db.session.commit()
         self.studentA['id'] = user.id
-        
+
         Enrollment.create(self.course.id, [self.studentA])
-        
+
         self.enrollment_matches_info(user, self.studentA)
-        
+
     def test_enroll_from_form(self):
         self.setup_course()
-        
+
         Enrollment.enroll_from_form(self.course.id, make_enrollment_form(self.studentB))
-        
+
         user = User.lookup(self.studentB['email'])
         self.studentB['id'] = user.id
-        
+
         self.enrollment_matches_info(user, self.studentB)
 
-        Enrollment.enroll_from_form(self.course.id, make_enrollment_form(self.lab_assistantA)) 
+        Enrollment.enroll_from_form(self.course.id, make_enrollment_form(self.lab_assistantA))
         lab_assistant = User.lookup(self.lab_assistantA['email'])
         self.lab_assistantA['id'] = lab_assistant.id
-        
+
         self.enrollment_matches_info(lab_assistant, self.lab_assistantA)
 
-        
+
     def test_enroll_from_csv(self):
         self.setup_course()
-        
+
         template = "{email},{name},{sid},{class_account},{section}"
         form = BatchEnrollmentForm()
         form.csv.data = template.format(**self.studentA) + "\n" + template.format(**self.studentB)
-        
+
         Enrollment.enroll_from_csv(self.course.id, form)
-        
+
         userA = User.lookup(self.studentA['email'])
         self.studentA['id'] = userA.id
         self.enrollment_matches_info(userA, self.studentA)
-        
+
         userB = User.lookup(self.studentB['email'])
         self.studentB['id'] = userB.id
         self.enrollment_matches_info(userB, self.studentB)
-        
+
     def test_enroll_twice(self):
         self.setup_course()
-        
+
         form = make_enrollment_form(self.studentB)
         Enrollment.enroll_from_form(self.course.id, form)
-        
+
         user = User.lookup(self.studentB['email'])
         self.studentB['id'] = user.id
-        
+
         self.enrollment_matches_info(user, self.studentB)
-        
+
         form = make_enrollment_form(self.studentB_alt)
         Enrollment.enroll_from_form(self.course.id, form)
-        
+
         user_updated = User.lookup(self.studentB['email'])
         self.studentB_alt['id'] = user_updated.id
         assert user.id == user_updated.id
-        
+
         self.enrollment_matches_info(user, self.studentB_alt)
-        
+
     def enrollment_matches_info(self, user, info):
         query = Enrollment.query.filter_by(
             user=user,
             course=self.course
         )
         assert query.count() == 1
-        
+
         enrollment = query[0]
         assert enrollment.user.name == info.get('name')
         assert enrollment.user.email == info.get('email')
@@ -133,7 +133,7 @@ class TestEnrollment(OkTestCase):
         [db.session.delete(e) for e in (Enrollment.query
                             .options(db.joinedload('user'))
                             .filter_by(role = LAB_ASSISTANT_ROLE,
-                                    course_id = self.course_id)
+                                    course_id = self.course.id)
                             .all()
                             )]
         db.session.commit()
@@ -168,7 +168,7 @@ class TestEnrollment(OkTestCase):
         response = self.client.post('/admin/course/{}/enrollment'.format(self.course.id),
                         data=self.lab_assistantB)
         self.assertRedirects(response, '/')
-        
+
 def make_enrollment_form(info):
     form = EnrollmentForm()
     form.name.data = info.get('name')
@@ -178,4 +178,3 @@ def make_enrollment_form(info):
     form.section.data = info.get('section')
     form.role.data = info.get('role')
     return form
-    
