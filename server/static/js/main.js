16127ff49859ada75291e5f8562429be8eab6108
Sumukh Sridhara <sumukh1@gmail.com>
2016-10-03 19:29:04 -0700

    Generate assignment names

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -34,3 +34,19 @@ $('body').on('click', 'button[data-confirm]', function(e) {
       if (isConfirm) form.submit();
   });
 })
+
+// From: http://davidwalsh.name/javascript-debounce-function
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
