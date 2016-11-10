6e50e75bb20d561d72bf922135c121a5af08d970
Kyle Raftogianis <knrafto@gmail.com>
2016-11-09 16:13:52 -0800

    Add checkpoint score kinds

--- server/constants.py
+++ server/constants.py
@@ -16,7 +16,9 @@ VALID_ROLES = [STUDENT_ROLE, LAB_ASSISTANT_ROLE, GRADER_ROLE, STAFF_ROLE,
                INSTRUCTOR_ROLE]
 STAFF_ROLES = [GRADER_ROLE, STAFF_ROLE, INSTRUCTOR_ROLE]
 SCORE_KINDS = ['composition', 'correctness', 'total', 'partner a', 'partner b',
-               'regrade', 'revision', 'private', 'autograder']
+               'regrade', 'revision',
+               'checkpoint 1', 'checkpoint 2',
+               'private', 'autograder']
 API_PREFIX = '/api'
 OAUTH_SCOPES = ['all', 'email']
 
