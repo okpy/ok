602deda853147a12823e614643b5a7fbfc9d6e1c
Sumukh Sridhara <sumukh1@gmail.com>
2016-12-21 00:15:47 -0800

    Fix modelproxy to return actual model instead of module

--- server/models/proxy.py
+++ server/models/proxy.py
@@ -1,6 +1,8 @@
+import server.models.model
+
 class ModelProxy(object):
     def __getattribute__(self, key):
-        import server.models.model
-        return server.models.model.__getattribute__(key)
+        module = server.models.model.__getattribute__(key)
+        return module.__getattribute__(key)
 
 ModelProxy = ModelProxy()
