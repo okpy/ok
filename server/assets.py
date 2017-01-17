8cab02a09e7d10463f89d4408f37521d83f85934
Sumukh Sridhara <sumukh1@gmail.com>
2017-01-17 15:59:18 -0800

    Notebook viewer (#1032)
    * Fallback to the textlexer so that pygments can escape files
    
    * First version of client side ipython rendering
    
    * Static files for rendering
    
    * Remove spinner and prototype safe markdown rendering
    
    * Render notebook with anchors
    
    * Add ipython rendering to staff view as well
    
    * Markdown should use helvetica
    
    * Keep keepends=True
    
    * Include a sample ipynb file
    
    * Show anchor link and keep fonts consistent
    
    * Make no highlight test adapt to using text lexer
    
    * Add default args for File obj
    
    * Allow 2MB code rendering (for large notebooks
    
    * Generate some notebook in seed files as well
    
    * Use empty set as default for lines in File obj

--- server/assets.py
+++ server/assets.py
@@ -12,6 +12,8 @@ common_css = Bundle(
     'css/highlight.css',
     'css/code.css',
     'css/swal-theme.css',
+    'css/notebook.css',
+    'lib/notebookjs/notebook.css',
     filters='cssmin',
     output='public/css/common.css'
 )
@@ -25,6 +27,9 @@ oauth_css = Bundle(
 
 common_js = Bundle(
     'js/main.js',
+    'js/notebook.js',
+    'lib/notebookjs/notebook.min.js',
+    'lib/notebookjs/ansi_up.min.js',
     filters='jsmin',
     output='public/js/common.js'
 )
