4c082c2128f637085b8c21a96c721002613f8859
Sumukh Sridhara <sumukh1@gmail.com>
2017-01-17 01:17:28 -0800

    Static files for rendering

---
+++ server/static/js/notebook.js
@@ -0,0 +1,10 @@
+function renderNotebook(ipynb) {
+    var data = JSON.parse(ipynb);
+    var notebook = nb.parse(data);
+    return notebook.render();
+}
+
+function displayNotebooks(ipyFiles) {
+
+
+}
