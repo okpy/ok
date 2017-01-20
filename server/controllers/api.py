4136ace897b6db358c4f95760dad7d1ca50b11a1
Kyle Raftogianis <knrafto@gmail.com>
2017-01-20 01:49:54 -0800

    Do not check client version (#1040)

--- server/controllers/api.py
+++ server/controllers/api.py
@@ -131,35 +131,6 @@ def check_scopes(func):
         return func(*args, **kwargs)
     return wrapper
 
-def check_version(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        supplied = request.args.get('client_version')
-        # ok-client doesn't send client_name right now
-        client = request.args.get('client_name', 'ok-client')
-        current_version, download_link = models.Version.get_current_version(
-            client)
-        if not supplied or supplied == current_version:
-            return func(*args, **kwargs)
-
-        message = ("Incorrect client version. Supplied version was {}. " +
-                   "Correct version is {}.").format(supplied, current_version)
-        data = {
-            'supplied': supplied,
-            'correct': current_version,
-            'download_link': download_link
-        }
-
-        response = jsonify(**{
-            'status': 403,
-            'message': message,
-            'data': data
-        })
-        response.status_code = 403
-        return response
-    return wrapper
-
-
 def make_backup(user, assignment_id, messages, submit):
     """
     Create backup with message objects.
@@ -454,7 +425,7 @@ class CommentSchema(APISchema):
 
 class Resource(restful.Resource):
     version = 'v3'
-    method_decorators = [check_scopes, authenticate, check_version]
+    method_decorators = [check_scopes, authenticate]
     required_scopes = {}
     # applies to all inherited resources
 
