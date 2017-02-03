606ba684c2a4eda36eb2ccf3d177096ffc58380c
Sumukh Sridhara <sumukh1@gmail.com>
2017-02-03 13:53:02 -0800

    Charts to side layout

--- server/controllers/admin.py
+++ server/controllers/admin.py
@@ -529,19 +529,21 @@ def view_scores(cid, aid):
     distinct_scores = set(sum(score_distribution.values(), []))
     sorted_scores = sorted(list(distinct_scores))
 
-    bar_chart = pygal.Bar()
-    bar_chart.x_labels = [str(x) for x in sorted_scores]
+    bar_charts = {}
 
     for kind, distribution in score_distribution.items():
         score_counts = collections.Counter(distribution)
+        bar_chart = pygal.Bar()
+        bar_chart.title = '{} distribution ({} items)'.format(kind, len(distribution))
         bar_chart.add(kind, [score_counts.get(x) for x in sorted_scores])
+        bar_chart.x_labels = [str(x) for x in sorted_scores]
+        bar_charts[kind] = bar_chart.render().decode("utf-8")
 
-    bar_chart = bar_chart.render().decode("utf-8")
 
     return render_template('staff/course/assignment/assignment.scores.html',
                            assignment=assign, current_course=current_course,
                            courses=courses, scores=all_scores,
-                           scores_plot=bar_chart)
+                           score_plots=bar_charts)
 
 @admin.route("/course/<int:cid>/assignments/<int:aid>/scores/audit")
 @is_staff(course_arg='cid')
