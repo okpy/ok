2ba0121024fca8de52588fb792f9e190bea72505
Sumukh Sridhara <sumukh1@gmail.com>
2017-02-24 22:29:58 -0800

    Replace full anonmyized history with anonmyized final submission export
    Fix anon formatting

--- server/jobs/export.py
+++ server/jobs/export.py
@@ -12,21 +12,15 @@ from server.utils import encode_id, local_time
 def student_hash(email):
     return hashlib.md5(email.strip().lower().encode()).hexdigest()
 
-def anonymize_backup(back, course, logger):
-    user_email = User.email_by_id(back.submitter_id)
-    data = {
-        'submitter': student_hash(user_email),
-        'messages': {m.kind: m.contents for m in back.messages},
-        'created': local_time(back.created, course),
-    }
-    # Ensure email is not in any of the messages
-    for kind, message in data['messages'].items():
-        if user_email and user_email.lower() in str(message).lower():
-            logger.warning("Found contact information in backup {}. Ignoring".format(back.hashid))
-            return {'submitter': student_hash(user_email)}
-    return data
+def pii_present(files, email, course, logger):
+    """ Ensure the student's email is not in any of the files. """
+    email = email.lower()
+    for name, contents in files.items():
+        if email in contents.lower() or email in name.lower():
+            logger.warning("Found contact information in a submission. Ignoring".format(name))
+            return True
 
-def write_final_submission(zf, logger, assignment, student, seen):
+def write_final_submission(zf, logger, assignment, student, seen, anonymize):
     """ Get the final submission STUDENT and write it into the zipfile ZF. """
     student_user = student.user
     stats = assignment.user_status(student_user)
@@ -37,47 +31,39 @@ def write_final_submission(zf, logger, assignment, student, seen):
         group_emails = [User.email_by_id(m.user_id) for m in stats.group.members]
     else:
         group_emails = [student_user.email]
+
     group_str = '-'.join(sorted(group_emails))
     if group_str in seen:
         return
     seen.add(group_str)
-    folder = "{}/{}/{}".format(assignment.name.replace('/', '-'),
-                               group_str, backup.hashid)
-    course = assignment.course
-    dump_info = {
-        'group': group_emails,
-        'scores': [s.export for s in stats.scores],
-        'submitter': User.email_by_id(backup.submitter_id),
-        'subm_time_local': local_time(stats.subm_time, course)
-    }
-    if backup.custom_submission_time:
-        dump_info['custom_time_local'] = local_time(backup.custom_submission_time,
-                                                    course)
 
-    zf.writestr("{}/info.json".format(folder), json.dumps(dump_info))
-    for name, contents in backup.files().items():
+    course = assignment.course
+    files = backup.files()
+
+    if anonymize:
+        for email in group_emails:
+            if pii_present(files, email, assignment.course, logger):
+                return
+        identifier = '-'.join(sorted([student_hash(e) for e in group_emails]))
+        folder = "{}/{}/".format(assignment.name.replace('/', '-'), identifier)
+    else:
+        folder = "{}/{}/{}".format(assignment.name.replace('/', '-'),
+                                   group_str, backup.hashid)
+        dump_info = {
+            'group': group_emails,
+            'scores': [s.export for s in stats.scores],
+            'submitter': User.email_by_id(backup.submitter_id),
+            'subm_time_local': local_time(stats.subm_time, course)
+        }
+        if backup.custom_submission_time:
+            dump_info['custom_time_local'] = local_time(backup.custom_submission_time,
+                                                        course)
+
+        zf.writestr("{}/info.json".format(folder), json.dumps(dump_info))
+
+    for name, contents in files.items():
         # Write the file to the in-memory zip
         zf.writestr("{}/{}".format(folder, name), contents)
-
-
-def write_anon_backups(zf, logger, assignment, student, seen):
-    """ Get all backups for STUDENT and write it into the zipfile ZF. """
-    if student.user.id in seen:
-        return
-    group_ids = assignment.active_user_ids(student.user.id)
-    seen |= group_ids
-
-    backups = (Backup.query
-                     .filter(Backup.submitter_id.in_(group_ids),
-                             Backup.assignment_id == assignment.id)
-                     .order_by(Backup.created.desc()))
-
-    course = assignment.course
-    student_history = [anonymize_backup(b, course, logger) for b in backups]
-
-    zf.writestr("anon-{}/{}/backups.json".format(assignment.name.replace('/', '-'),
-                                                 student_hash(student.user.email)),
-                json.dumps(student_history))
 
 def export_loop(bio, zf, logger, assignment, anonymize):
     course = assignment.course
@@ -85,11 +71,7 @@ def export_loop(bio, zf, logger, assignment, anonymize):
     seen = set()
     num_students = len(enrollments)
     for index, student in enumerate(enrollments):
-        if anonymize:
-            write_anon_backups(zf, logger, assignment, student, seen)
-        else:
-            write_final_submission(zf, logger, assignment, student, seen)
-
+        write_final_submission(zf, logger, assignment, student, seen, anonymize)
         # Rough progress report
         percent_complete = ((index+1)/num_students) * 100
         if round(percent_complete, 1) % 5 == 0:
@@ -104,8 +86,8 @@ def export_assignment(assignment_id, anonymized):
 
     Final Submission: One submission per student/group
         Zip Strucutre: cal-cs61a../s1@a.com-s2@b.com/abc12d/hog.py
-    Anonymized: All backups without identifying info
-        Zip Strucutre: anon-cal-cs61a../{hash}/backups.json
+    Anonymized: Submission without identifying info
+        Zip Strucutre: cal-cs61a../{hash}/hog.py
     """
     logger = jobs.get_job_logger()
 
@@ -120,9 +102,9 @@ def export_assignment(assignment_id, anonymized):
         raise Exception("{} does not have enough permission"
                         .format(requesting_user.email))
     if anonymized:
-        logger.info("Starting anonymized backup")
+        logger.info("Starting anonymized submission export")
     else:
-        logger.info("Start final submission export")
+        logger.info("Starting final submission export")
     course = assignment.course
     with io.BytesIO() as bio:
         # Get a handle to the in-memory zip in append mode
@@ -136,8 +118,8 @@ def export_assignment(assignment_id, anonymized):
         # Close zf handle to finish writing zipfile
         logger.info("Uploading...")
         upload = ExternalFile.upload(bio, user_id=requesting_user.id, name=zip_name,
-                                        course_id=course.id,
-                                        prefix='research/exports/{}/'.format(course.offering))
+                                     course_id=course.id,
+                                     prefix='jobs/exports/{}/'.format(course.offering))
 
     logger.info("Saved as: {0}".format(upload.object_name))
     msg = "/files/{0}".format(encode_id(upload.id))
