01cfb2ae1e6a2a9f21d90ea8509124b947d20994
Kyle Raftogianis <knrafto@gmail.com>
2017-02-28 03:29:01 -0800

    Do not cache during tests

--- server/settings/test.py
+++ server/settings/test.py
@@ -30,7 +30,7 @@ STORAGE_CONTAINER = os.path.abspath("./local-storage")
 if not os.path.exists(STORAGE_CONTAINER):
     os.makedirs(STORAGE_CONTAINER)
 
-CACHE_TYPE = 'simple'
+CACHE_TYPE = 'null'
 
 RQ_DEFAULT_HOST = REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
 REDIS_PORT = 6379
