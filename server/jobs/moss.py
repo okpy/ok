f5a1c72793aa76ee66383405f884314d5b0b78fb
Jack Thakar <jathak@gmail.com>
2017-02-27 23:51:19 -0800

    Multi-file support; recalculated similarity
    Multiple files are now concatenated into a single one to send to Moss, with the
    results transformed back to per-file line numbers.
    
    The parser now discards Moss's similarity percent and instead recalculates it
    as [# of lines in Moss result but not in template / # of lines not in template]
    
    The time the result was created is now shown in the table.

--- server/jobs/moss.py
+++ server/jobs/moss.py
@@ -11,6 +11,7 @@ from html.parser import HTMLParser
 
 from server.models import Assignment, Backup, MossResult, db
 from server.utils import encode_id, decode_id
+from server.highlight import highlight_diff
 from server import jobs
 
 def moss_submit(moss_id, submissions, ref_submissions, language,
@@ -50,9 +51,12 @@ def moss_submit(moss_id, submissions, ref_submissions, language,
                           .all())
     if template_files:
         logger.info('Uploading template...')
+        merged_contents = ""
         for filename in template_files:
-            contents = template_files[filename]
-            send_file(moss, filename, contents, 0, language)
+            if filename == 'submit' or not match_pattern.match(filename):
+                continue
+            merged_contents += template_files[filename] + '\n'
+        send_file(moss, 'allcode', merged_contents, 0, language)
     fid = 0
     logger.info('Uploading submissions...')
     for backup in backup_query:
@@ -61,21 +65,23 @@ def moss_submit(moss_id, submissions, ref_submissions, language,
             logger.info("{} didn't have any file contents".format(backup.hashid))
             continue
         contents = file_contents[0].contents
-        for filename in contents:
+        merged_contents = ""
+        for filename in sorted(contents.keys()):
             if filename == 'submit' or not match_pattern.match(filename):
                 continue
-            fid += 1
-            path = os.path.join(backup.hashid, filename)
-            send_file(moss, path, contents[filename], fid, language)
+            merged_contents += contents[filename] + '\n'
+        fid += 1
+        path = os.path.join(backup.hashid, 'allcode')
+        send_file(moss, path, merged_contents, fid, language)
     moss.send("query 0 Submitted via okpy.org\n".encode())
     logger.info('Awaiting response...')
     url = moss.recv(1024).decode().strip()
     moss.send("end\n".encode())
     moss.close()
     logger.info('Moss results at: {}'.format(url))
-    parse_moss_results(url, hashed_subm_keys, logger)
+    parse_moss_results(url, hashed_subm_keys, logger, match_pattern, template_files)
 
-def parse_moss_results(base_url, hashed_ids, logger):
+def parse_moss_results(base_url, hashed_ids, logger, pattern, template):
     match = 0
     while True:
         r = requests.get('{}/match{}-top.html'.format(base_url, match))
@@ -95,12 +101,16 @@ def parse_moss_results(base_url, hashed_ids, logger):
         matchesB = [[int(i) for i in r.split('-')] for r in rangesB]
         submissionA = Backup.query.filter_by(id=decode_id(hashidA)).one_or_none()
         submissionB = Backup.query.filter_by(id=decode_id(hashidB)).one_or_none()
+        matchesA = recalculate_lines(submissionA, matchesA, pattern)
+        matchesB = recalculate_lines(submissionB, matchesB, pattern)
+        similarityA = recalculate_similarity(submissionA, matchesA, template)
+        similarityB = recalculate_similarity(submissionB, matchesB, template)
         resultA = MossResult.query.filter_by(submissionA=submissionA, submissionB=submissionB,
             similarityA=similarityA, similarityB=similarityB,
-            matchesA=matchesA, matchesB=matchesB)
+            matchesA=matchesA, matchesB=matchesB).one_or_none()
         resultB = MossResult.query.filter_by(submissionB=submissionA, submissionA=submissionB,
             similarityB=similarityA, similarityA=similarityB,
-            matchesB=matchesA, matchesA=matchesB)
+            matchesB=matchesA, matchesA=matchesB).one_or_none()
         if resultA or resultB:
             logger.info('Moss result #{} already exists.'.format(match))
             continue
@@ -110,8 +120,6 @@ def parse_moss_results(base_url, hashed_ids, logger):
         db.session.add(result)
         logger.info('Adding Moss result #{}...'.format(match))
     db.session.commit()
-    moss_results = MossResult.query.all()
-    logger.info('Total: {}'.format(len(moss_results)))
 
 class MossParser(HTMLParser):
     def __init__(self):
@@ -131,6 +139,51 @@ class MossParser(HTMLParser):
         elif self.last_start == 'a' and data:
             self.ranges.append(data)
 
+def recalculate_lines(submission, raw_matches, pattern):
+    files = submission.files()
+    file_matches = {f:[] for f in files if pattern.match(f)}
+    starts = {}
+    current = 0
+    for filename in sorted(file_matches.keys()):
+        starts[current] = filename
+        current += len(files[filename].split('\n'))
+    def find_file(line):
+        last_line = 0
+        for start_line in starts:
+            if line == start_line:
+                return starts[start_line], start_line
+            if line < start_line:
+                return starts[last_line], last_line
+            last_line = start_line
+        return starts[last_line], last_line
+    for match in raw_matches:
+        start_file, sf_line = find_file(match[0])
+        end_file, ef_line = find_file(match[1])
+        if start_file == end_file:
+            file_matches[start_file].append([x - sf_line for x in match])
+        else:
+            start_match = [match[0] - sf_line, len(files[filename].split('\n')) - 1]
+            file_matches[start_file].append(start_match)
+            end_match = [0, match[1] - ef_line]
+            file_matches[end_file].append(end_match)
+    return file_matches
+
+def recalculate_similarity(submission, matches, template):
+    student_lines, similar_student_lines = 0, 0
+    files = submission.files()
+    for filename in matches:
+        student_code = files[filename]
+        matched_lines = []
+        for m in matches[filename]:
+            matched_lines += list(range(m[0], m[1] + 1))
+        matched_lines = set(matched_lines)
+        template_code = template[filename] if filename in template else ""
+        for line in highlight_diff(filename, template_code, student_code):
+            if line.tag == 'insert':
+                student_lines += 1
+                if line.line_after in matched_lines:
+                    similar_student_lines += 1
+    return round(similar_student_lines * 100 / student_lines)
 
 def send_file(moss, path, contents, fid, language):
     size = len(contents.encode())
@@ -153,5 +206,6 @@ def submit_to_moss(moss_id=None, file_regex=".*", assignment_id=None, language=N
     return moss_submit(moss_id, subms, [], language, template, 10, file_regex)
 
 @jobs.background_job
-def parse_moss_job(base_url, hashed_ids=None):
-    parse_moss_results(base_url, hashed_ids, jobs.get_job_logger())
+def parse_moss_job(base_url, hashed_ids=None, file_regex='.*', template={}):
+    parse_moss_results(base_url, hashed_ids, jobs.get_job_logger(),
+                       sre.compile(file_regex), template)
