cbddc9419e392fc8e78c930a769017abbf2d23e9
Colin Schoen <cschoen@berkeley.edu>
2017-03-01 16:08:31 -0800

    Fix second instance of camel case
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- server/forms.py
+++ server/forms.py
@@ -439,8 +439,8 @@ class EditClientForm(ClientForm):
         if not super(ClientForm, self).validate():
             return False
         if self.client_id.data != self.obj.client_id:
-            existingClient = Client.query.filter_by(client_id=self.client_id.data).first()
-            if existingClient:
+            existing_client = Client.query.filter_by(client_id=self.client_id.data).first()
+            if existing_client:
                 self.client_id.errors.append('That client ID already exists')
                 return False
         return True
