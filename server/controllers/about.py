91dd89f22d3e15b342e9b8c9d68d23e18313a5da
Sumukh Sridhara <sumukh1@gmail.com>
2017-03-04 17:21:00 -0800

    Documentation -> /about/documentation'

--- server/controllers/about.py
+++ server/controllers/about.py
@@ -1,10 +1,15 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
 
 about = Blueprint('about', __name__)
 
 @about.route('/privacy/')
 def privacy():
     return render_template('about/privacy.html')
+
+@about.route('/documentation/')
+@about.route('/docs/')
+def documentation():
+    return redirect('https://okpy.github.io/documentation/')
 
 @about.route('/publications/')
 def research():
