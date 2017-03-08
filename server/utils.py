bdbe71c654cc7dfb57e10c1db976a43b180ac476
Sumukh Sridhara <sumukh1@gmail.com>
2017-03-07 16:53:23 -0800

    Landing Page Improvements & Course Creation by any user (#1107)
    * Update sentry config
    
    * Add Feature block
    
    * cut descriptions on text and add pricing section
    
    * Some copy improvements
    
    * Hide/show demo video
    
    * Landing video buttons
    
    * Add Publications to Footer
    
    * Documentation -> /about/documentation'
    
    * Make oauth/client versions require admin
    
    * Allow any user to create a course. Send welcome emails
    
    * fix dividers if you do not have any active courses
    
    * Add student data warning to Oauth for staff members
    
    * Include denero in course creation email
    
    * Show demo by default, button movements/copy updates
    
    * Add a checklist for getting started
    
    * Include students table first; small copy/style changes to first time info
    
    * Add tests for is_staff and create_course redirect
    
    * link to student interface
    
    * Add test for admin only ability to edit versions
    
    * Add tests for oauth client permissions
    
    * Add STAFF_ROLES back to test_enrollment
    
    * Remove extra user in test
    
    * Use first name in email template
    
    * Github link in about
    
    * Enforce valid names for courses to match format
    
    * Only admins are able to edit version via the api
    
    * Closing period for email template sentence
    
    * tests check that staff no longer can update api versions

--- server/utils.py
+++ server/utils.py
@@ -113,6 +113,11 @@ def natural_time(date):
     now = dt.datetime.utcnow()
     return humanize.naturaltime(now - date)
 
+def first_name(name):
+    """ Return the first name of a name."""
+    if not isinstance(name, str):
+        return name
+    return name.split(' ')[0].title()
 
 def humanize_name(name):
     """ Return a canonical representation of a name in First Last format."""
@@ -136,6 +141,20 @@ def random_row(query):
     if not count:
         return None
     return query.offset(random.randrange(count)).first()
+
+def new_course_email(instructor, course):
+    subject = "{} + OK".format(course.display_name)
+    template = 'email/new_course.html'
+    text = "" # The template already includes the copy
+    link_text = "View OK Documentation"
+    link = url_for('about.documentation', _external=True)
+    # use +ok in cc'd emails so that those users are still valid recipients
+    return send_email(instructor.email, subject, text,
+               reply_to="sumukh@berkeley.edu",
+               from_name="OK Team",
+               cc=('sumukh+ok@berkeley.edu', 'denero+ok@berkeley.edu'),
+               template=template, link_text=link_text, link=link,
+               course=course, instructor=instructor)
 
 def invite_email(member, recipient, assignment):
     subject = "{0} group invitation".format(assignment.display_name)
