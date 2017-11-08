582cfb2b62125e5c6384e5123b0550a63d938a58
Colin Schoen <cschoen@berkeley.edu>
2017-11-08 15:36:10 -0800

    Allow any SSL protocol when using PhantomJS
    Signed-off-by: Colin Schoen <cschoen@berkeley.edu>

--- tests/test_web.py
+++ tests/test_web.py
@@ -19,7 +19,7 @@ from server.constants import STAFF_ROLES
 from tests import OkTestCase
 
 try:
-    driver = webdriver.PhantomJS()
+    driver = webdriver.PhantomJS(service_args=['--ssl-protocol=any'])
 except:
     print("PhantomJS is not installed. Not running integration tests")
     driver = None
