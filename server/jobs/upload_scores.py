0616dd5e12306acb7e5141f5f2928eb8c21614e8
Eric Pai <ericpai@berkeley.edu>
2017-12-14 01:28:54 -0800

    fix multiple backups being created

--- server/jobs/upload_scores.py
+++ server/jobs/upload_scores.py
@@ -14,7 +14,7 @@ from traceback import print_exc
 
 
 @jobs.background_job
-def score_from_csv(assign_id, rows, kind='total', invalid=None):
+def score_from_csv(assign_id, rows, kind='total', invalid=None, message=None):
     """
     Job for uploading Scores.
 
@@ -24,6 +24,8 @@ def score_from_csv(assign_id, rows, kind='total', invalid=None):
     log = jobs.get_job_logger()
     current_user = jobs.get_current_job().user
     assign = Assignment.query.get(assign_id)
+
+    message = message or '{} score for {}'.format(kind.title(), assign.display_name)
 
     def log_err(msg):
         log.info('\t!  {}'.format(msg))
@@ -41,11 +43,17 @@ def score_from_csv(assign_id, rows, kind='total', invalid=None):
         try:
             email, score = row['email'], row['score']
             user = User.query.filter_by(email=email).one()
-            backup = Backup.create(submitter=user, assignment_id=assign.id, submit=True)
-            uploaded_score = Score(grader_id=current_user.id, assignment=backup.assignment,
-                        backup=backup, user_id=backup.submitter_id, score=score, kind=kind)
+
+            backup = Backup.query.filter_by(assignment=assign, submitter=user, submit=True).first()
+            if not backup:
+                backup = Backup.create(submitter=user, assignment=assign, submit=True)
+
+            uploaded_score = Score(grader=current_user, assignment=assign,
+                    backup=backup, user=user, score=score, kind=kind, message=message)
+
             db.session.add(uploaded_score)
             uploaded_score.archive_duplicates()
+
         except SQLAlchemyError:
             print_exc()
             log_err('error: user with email `{}` does not exist'.format(email))
