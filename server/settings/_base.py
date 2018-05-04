okpy/ok

commit ec06b9ce110143733a6b153978aa38f8f1efa989

Refactoring the settings config to remove duplicates (#1259)
There are many shared settings between the environment setting files, this
commit creates a base class that is shared across all environments; a prodbase
class which contains settings shared across simple, staging and prod; and a
devbase class which has settings shared across dev and test. The individual
env setting files then inherit from the bases and overwrite as needed.

server/settings/_base.py
@@ -1,0 +1,85 @@
+""" Do not put secrets in this file. This file is public.
+    Production config.
+"""
+import os
+
+from server.settings import RAVEN_IGNORE_EXCEPTIONS as raven_exceptions
+
+# Shared settings across all environments
+
+def initialize_config(cls):
+    cls.initialize_config()
+    return cls
+
+class Config(object):
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    
+    DEBUG = False
+    ASSETS_DEBUG = False
+    TESTING_LOGIN = False
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+    
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    REDIS_PORT = 6379
+    RQ_POLL_INTERVAL = 2000
+    RQ_DEFAULT_HOST = REDIS_HOST = CACHE_REDIS_HOST = \
+        os.getenv('REDIS_HOST', 'localhost')
+
+    STORAGE_SERVER = False
+
+    RAVEN_IGNORE_EXCEPTIONS = raven_exceptions
+
+    # Service Keys
+    GOOGLE = {
+        'consumer_key': os.environ.get('GOOGLE_ID'),
+        'consumer_secret':  os.environ.get('GOOGLE_SECRET')
+    }
+
+    SENDGRID_AUTH = {
+        'user': os.environ.get("SENDGRID_USER"),
+        'key': os.environ.get("SENDGRID_KEY")
+    }
+
+    @classmethod
+    def initialize_config(cls):
+        db_url = os.getenv('DATABASE_URL')
+        if db_url:
+            if 'mysql' in db_url:
+                db_url = db_url.replace('mysql://', 'mysql+pymysql://')
+                db_url += "&sql_mode=STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"
+
+                #This defends against people copy/pasting a connection URL that doesn't include any query parameters 
+                # (in which case the following code creates a bad URI)
+                if '?' not in db_url:
+                    db_url += '?'
+        else:
+            db_url = cls.get_default_db_url()
+
+        cls.SQLALCHEMY_DATABASE_URI = db_url
+
+        # If using sqlite use absolute path (otherwise we break migrations)
+        sqlite_prefix = 'sqlite:///'
+        if cls.SQLALCHEMY_DATABASE_URI.startswith(sqlite_prefix):
+            cls.SQLALCHEMY_DATABASE_URI = (sqlite_prefix +
+                    os.path.abspath(cls.SQLALCHEMY_DATABASE_URI[len(sqlite_prefix) + 1:]))
+
+
+        cls.STORAGE_PROVIDER = os.getenv('STORAGE_PROVIDER', 'LOCAL')
+        cls.STORAGE_CONTAINER = os.environ.get('STORAGE_CONTAINER', os.path.abspath("./local-storage"))
+        cls.STORAGE_KEY = os.environ.get('STORAGE_KEY', '')
+        cls.STORAGE_SECRET = os.environ.get('STORAGE_SECRET', '').replace('\\n', '\n')
+
+        if cls.STORAGE_PROVIDER == 'LOCAL' and not os.path.exists(cls.STORAGE_CONTAINER):
+            os.makedirs(cls.STORAGE_CONTAINER)
+
+        cls.verify_oauth_credentials()
+
+
+    @classmethod
+    def get_default_db_url(cls):
+        raise NotImplementedError
+
+    @classmethod
+    def verify_oauth_credentials(cls):
+        raise NotImplementedError

server/settings/_devbase.py
@@ -1,0 +1,14 @@
+# Shared settings across dev and test
+
+class Config(object):
+    ASSETS_DEBUG = True
+    TESTING_LOGIN = True
+    INSTANTCLICK = True
+
+    CACHE_TYPE = 'simple'
+
+    GOOGLE_CONSUMER_KEY = ''
+
+    @classmethod
+    def verify_oauth_credentials(cls):
+        pass

server/settings/_prodbase.py
@@ -1,0 +1,24 @@
+import os
+
+# Shared settings across prod, staging and simple
+
+class Config(object):
+    SENTRY_USER_ATTRS = ['email', 'name']
+    PREFERRED_URL_SCHEME = 'https'
+    OAUTH2_PROVIDER_TOKEN_EXPIRES_IN = 28800
+
+    WTF_CSRF_CHECK_DEFAULT = True
+    WTF_CSRF_ENABLED = True
+    
+    CACHE_TYPE = 'simple'
+    CACHE_KEY_PREFIX = 'ok-web'
+
+    RQ_DEFAULT_HOST = REDIS_HOST = CACHE_REDIS_HOST = \
+        os.getenv('REDIS_HOST', 'redis-master')
+
+    STORAGE_CONTAINER = os.environ.get('STORAGE_CONTAINER',  'ok-v3-user-files')
+
+    @classmethod
+    def verify_oauth_credentials(cls):
+        if "GOOGLE_ID" not in os.environ or "GOOGLE_SECRET" not in os.environ:
+            print("Warning: the google login variables are not set.")

server/settings/dev.py
@@ -1,47 +1,18 @@
 import os
 
-ENV = 'dev'
-SECRET_KEY = os.getenv('OK_SESSION_KEY', 'changeinproductionkey')
-CACHE_TYPE = 'simple'
+from server.settings._base import Config as BaseConfig, initialize_config
+from server.settings._devbase import Config as DevBaseConfig
 
-DEBUG = True
-ASSETS_DEBUG = True
-TESTING_LOGIN = True
-DEBUG_TB_INTERCEPT_REDIRECTS = False
-INSTANTCLICK = True
+@initialize_config
+class Config(DevBaseConfig, BaseConfig):
+    ENV = 'dev'
+    
+    SECRET_KEY = os.getenv('OK_SESSION_KEY', 'changeinproductionkey')
 
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-db_url = os.getenv('DATABASE_URL')
-if db_url:
-    if 'mysql' in db_url:
-        db_url = db_url.replace('mysql://', 'mysql+pymysql://')
-        db_url += "&sql_mode=STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"
-    SQLALCHEMY_DATABASE_URI = db_url
-else:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///../oksqlite.db'
-# SQLALCHEMY_ECHO = True
+    DEBUG = True
 
-# If using sqlite use absolute path (otherwise we break migrations)
-sqlite_prefix = 'sqlite:///'
-if SQLALCHEMY_DATABASE_URI.startswith(sqlite_prefix):
-    SQLALCHEMY_DATABASE_URI = (sqlite_prefix + 
-            os.path.abspath(SQLALCHEMY_DATABASE_URI[len(sqlite_prefix) + 1:]))
+    MAX_CONTENT_LENGTH = 20 * 1024 * 1024  # Max Upload Size is 20MB
 
-
-RQ_DEFAULT_HOST = REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
-RQ_POLL_INTERVAL = 2000
-
-MAX_CONTENT_LENGTH = 20 * 1024 * 1024  # Max Upload Size is 20MB
-
-STORAGE_PROVIDER = os.environ.get('STORAGE_PROVIDER',  'LOCAL')
-STORAGE_SERVER = False
-STORAGE_CONTAINER = os.environ.get('STORAGE_CONTAINER',
-                                   os.path.abspath("./local-storage"))
-STORAGE_KEY = os.environ.get('STORAGE_KEY', '')
-STORAGE_SECRET = os.environ.get('STORAGE_SECRET', '').replace('\\n', '\n')
-
-if STORAGE_PROVIDER == 'LOCAL' and not os.path.exists(STORAGE_CONTAINER):
-    os.makedirs(STORAGE_CONTAINER)
-
-GOOGLE_CONSUMER_KEY = ''
+    @classmethod
+    def get_default_db_url(cls):
+        return 'sqlite:///../oksqlite.db'

server/settings/prod.py
@@ -4,78 +4,27 @@
 import os
 import sys
 
-from server.settings import RAVEN_IGNORE_EXCEPTIONS
+from server.settings._base import Config as BaseConfig, initialize_config
+from server.settings._prodbase import Config as ProdBaseConfig
 
-ENV = 'prod'
-PREFERRED_URL_SCHEME = 'https'
+@initialize_config
+class Config(ProdBaseConfig, BaseConfig):
+    ENV = 'prod'
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+    STORAGE_PROVIDER = os.environ.get('STORAGE_PROVIDER',  'GOOGLE_STORAGE')
 
-DEBUG = False
-ASSETS_DEBUG = False
-TESTING_LOGIN = False
-DEBUG_TB_INTERCEPT_REDIRECTS = False
-# The Google Cloud load balancer behaves like two proxies: one with the external
-# fowarding rule IP, and one with an internal IP.
-# See https://cloud.google.com/compute/docs/load-balancing/http/#target_proxies
-# Including Nginx too makes 3 proxies.
-NUM_PROXIES = 3
+    MAX_CONTENT_LENGTH = 30 * 1024 * 1024  # Max Upload Size is 30MB
 
-db_url = os.getenv('DATABASE_URL')
-if db_url:
-    db_url = db_url.replace('mysql://', 'mysql+pymysql://')
-    db_url += "&sql_mode=STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"
-else:
-    print("The database URL is not set!")
-    sys.exit(1)
+    CACHE_TYPE = 'redis'
 
-SQLALCHEMY_DATABASE_URI = db_url
+    # The Google Cloud load balancer behaves like two proxies: 
+    # one with the external fowarding rule IP, and
+    # one with an internal IP.
+    # See https://cloud.google.com/compute/docs/load-balancing/http/#target_proxies
+    # Including Nginx too makes 3 proxies.
+    NUM_PROXIES = 3
 
-# If using sqlite use absolute path (otherwise we break migrations)
-sqlite_prefix = 'sqlite:///'
-if SQLALCHEMY_DATABASE_URI.startswith(sqlite_prefix):
-    SQLALCHEMY_DATABASE_URI = (sqlite_prefix +
-            os.path.abspath(SQLALCHEMY_DATABASE_URI[len(sqlite_prefix) + 1:]))
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SENTRY_USER_ATTRS = ['email', 'name']
-PREFERRED_URL_SCHEME = 'https'
-OAUTH2_PROVIDER_TOKEN_EXPIRES_IN = 28800
-
-WTF_CSRF_CHECK_DEFAULT = True
-WTF_CSRF_ENABLED = True
-
-MAX_CONTENT_LENGTH = 30 * 1024 * 1024  # Max Upload Size is 30MB
-
-CACHE_TYPE = 'redis'
-CACHE_KEY_PREFIX = 'ok-web'
-
-RQ_DEFAULT_HOST = REDIS_HOST = CACHE_REDIS_HOST = \
-    os.getenv('REDIS_HOST', 'redis-master')
-REDIS_PORT = 6379
-RQ_POLL_INTERVAL = 2000
-
-STORAGE_PROVIDER = os.environ.get('STORAGE_PROVIDER',  'GOOGLE_STORAGE')
-STORAGE_SERVER = False
-STORAGE_CONTAINER = os.environ.get('STORAGE_CONTAINER',  'ok-v3-user-files')
-STORAGE_KEY = os.environ.get('STORAGE_KEY', '')
-STORAGE_SECRET = os.environ.get('STORAGE_SECRET', '').replace('\\n', '\n')
-
-try:
-    os.environ["GOOGLE_ID"]
-    os.environ["GOOGLE_SECRET"]
-except KeyError:
-    print("Please set the google login variables.")
-    sys.exit(1)
-
-# Service Keys
-
-GOOGLE = {
-    'consumer_key': os.environ.get('GOOGLE_ID'),
-    'consumer_secret':  os.environ.get('GOOGLE_SECRET')
-}
-
-SENDGRID_AUTH = {
-    'user': os.environ.get("SENDGRID_USER"),
-    'key': os.environ.get("SENDGRID_KEY")
-}
+    @classmethod
+    def get_default_db_url(cls):
+        print("The database URL is not set!")
+        sys.exit(1)

server/settings/simple.py
@@ -2,74 +2,19 @@
     Production config.
 """
 import os
-import sys
 
-from server.settings import RAVEN_IGNORE_EXCEPTIONS
+from server.settings._base import Config as BaseConfig, initialize_config
+from server.settings._prodbase import Config as ProdBaseConfig
 
-ENV = 'simple'
-PREFERRED_URL_SCHEME = 'http'
+@initialize_config
+class Config(ProdBaseConfig, BaseConfig):
+    ENV = 'simple'
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+    PREFERRED_URL_SCHEME = 'http'
 
-DEBUG = False
-ASSETS_DEBUG = False
-TESTING_LOGIN = False
-DEBUG_TB_INTERCEPT_REDIRECTS = False
+    RQ_DEFAULT_HOST = REDIS_HOST = CACHE_REDIS_HOST = \
+        os.getenv('REDIS_HOST', 'localhost')
 
-db_url = os.getenv('DATABASE_URL')
-if db_url:
-    if 'mysql' in db_url:
-        db_url = db_url.replace('mysql://', 'mysql+pymysql://')
-        db_url += "&sql_mode=STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"
-else:
-    print("The database URL is not set!")
-    db_url = os.getenv('SQLALCHEMY_URL', 'sqlite:///../oksqlite.db')
-
-SQLALCHEMY_DATABASE_URI = db_url
-
-# If using sqlite use absolute path (otherwise we break migrations)
-sqlite_prefix = 'sqlite:///'
-if SQLALCHEMY_DATABASE_URI.startswith(sqlite_prefix):
-    SQLALCHEMY_DATABASE_URI = (sqlite_prefix +
-            os.path.abspath(SQLALCHEMY_DATABASE_URI[len(sqlite_prefix) + 1:]))
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SENTRY_USER_ATTRS = ['email', 'name']
-PREFERRED_URL_SCHEME = 'https'
-OAUTH2_PROVIDER_TOKEN_EXPIRES_IN = 28800
-
-WTF_CSRF_CHECK_DEFAULT = True
-WTF_CSRF_ENABLED = True
-
-CACHE_TYPE = 'simple'
-CACHE_KEY_PREFIX = 'ok-web'
-
-RQ_DEFAULT_HOST = REDIS_HOST = CACHE_REDIS_HOST = \
-    os.getenv('REDIS_HOST', '127.0.0.1')
-REDIS_PORT = 6379
-RQ_POLL_INTERVAL = 2000
-
-STORAGE_PROVIDER = 'LOCAL'
-STORAGE_SERVER = False
-STORAGE_CONTAINER = os.path.abspath("./local-storage")
-
-if not os.path.exists(STORAGE_CONTAINER):
-    os.makedirs(STORAGE_CONTAINER)
-try:
-    os.environ["GOOGLE_ID"]
-    os.environ["GOOGLE_SECRET"]
-except KeyError:
-    print("Please set the google login variables.")
-    sys.exit(1)
-
-# Service Keys
-
-GOOGLE = {
-    'consumer_key': os.environ.get('GOOGLE_ID'),
-    'consumer_secret':  os.environ.get('GOOGLE_SECRET')
-}
-
-SENDGRID_AUTH = {
-    'user': os.environ.get("SENDGRID_USER"),
-    'key': os.environ.get("SENDGRID_KEY")
-}
+    @classmethod
+    def get_default_db_url(cls):
+        return os.getenv('SQLALCHEMY_URL', 'sqlite:///../oksqlite.db')

server/settings/staging.py
@@ -2,74 +2,16 @@
     For staging environment (Using Dokku)
 """
 import os
-import sys
 
-from server.settings import RAVEN_IGNORE_EXCEPTIONS
+from server.settings._base import Config as BaseConfig, initialize_config
+from server.settings._prodbase import Config as ProdBaseConfig
 
-ENV = 'staging'
-PREFERRED_URL_SCHEME = 'https'
+@initialize_config
+class Config(ProdBaseConfig, BaseConfig):
+    ENV = 'staging'
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+    MAX_CONTENT_LENGTH = 10 * 1024 * 1024  # Max Upload Size is 10MB
 
-CACHE_TYPE = 'simple'
-
-DEBUG = False
-ASSETS_DEBUG = False
-TESTING_LOGIN = False
-DEBUG_TB_INTERCEPT_REDIRECTS = False
-
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SENTRY_USER_ATTRS = ['email', 'name']
-
-RQ_DEFAULT_HOST = REDIS_HOST = CACHE_REDIS_HOST = \
-    os.getenv('REDIS_HOST', 'redis-master')
-REDIS_PORT = 6379
-RQ_POLL_INTERVAL = 2000
-OAUTH2_PROVIDER_TOKEN_EXPIRES_IN = 28800
-
-db_url = os.getenv('DATABASE_URL')
-if db_url:
-    db_url = db_url.replace('mysql://', 'mysql+pymysql://')
-    db_url += "&sql_mode=STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION"
-
-else:
-    db_url = os.getenv('SQLALCHEMY_URL', 'sqlite:///../oksqlite.db')
-
-SQLALCHEMY_DATABASE_URI = db_url
-
-# If using sqlite use absolute path (otherwise we break migrations)
-sqlite_prefix = 'sqlite:///'
-if SQLALCHEMY_DATABASE_URI.startswith(sqlite_prefix):
-    SQLALCHEMY_DATABASE_URI = (sqlite_prefix +
-            os.path.abspath(SQLALCHEMY_DATABASE_URI[len(sqlite_prefix) + 1:]))
-
-
-WTF_CSRF_CHECK_DEFAULT = True
-WTF_CSRF_ENABLED = True
-MAX_CONTENT_LENGTH = 10 * 1024 * 1024  # Max Upload Size is 10MB
-
-STORAGE_PROVIDER = os.environ.get('STORAGE_PROVIDER',  'LOCAL')
-STORAGE_SERVER = False
-STORAGE_CONTAINER = os.environ.get('STORAGE_CONTAINER',  'ok-v3-user-files')
-STORAGE_KEY = os.environ.get('STORAGE_KEY', '')
-STORAGE_SECRET = os.environ.get('STORAGE_SECRET', '').replace('\\n', '\n')
-
-if STORAGE_PROVIDER == 'LOCAL' and not os.path.exists(STORAGE_CONTAINER):
-    os.makedirs(STORAGE_CONTAINER)
-
-try:
-    os.environ["GOOGLE_ID"]
-    os.environ["GOOGLE_SECRET"]
-except KeyError:
-    print("Please set the google login variables. source secrets.sh")
-    sys.exit(1)
-
-GOOGLE = {
-    'consumer_key': os.environ.get('GOOGLE_ID'),
-    'consumer_secret':  os.environ.get('GOOGLE_SECRET')
-}
-
-SENDGRID_AUTH = {
-    'user': os.environ.get("SENDGRID_USER"),
-    'key': os.environ.get("SENDGRID_KEY")
-}
+    @classmethod
+    def get_default_db_url(cls):
+        return os.getenv('SQLALCHEMY_URL', 'sqlite:///../oksqlite.db')

.gitignore
@@ -45,3 +45,6 @@ _cache/*
 database.db
 *.db
 *.db-journal
+
+# Visual Studio Code
+.vscode

commit 24681abd14285944a370e294981f353fca60e2ad

Ensure that all settings match old values
This fixes a regression introduced in ec06b9c where the settings were
refactored into a class hierarchy but some values were changed by
accident as part of this refactor.

The fix was verified by logging out the configs on the commit before the
refactor, applying the refactor, logging out the configs again and then
diffing the results.

Snippet used to log the configs:

```py
with open('config-' + app.config['ENV'] + '.txt', 'w') as fobj:
    config = str(app.config).replace(', ', ',\n')
    fobj.write(config)
```

Paired with @liliankasem

server/settings/_base.py
@@ -3,8 +3,6 @@
 """
 import os
 
-from server.settings import RAVEN_IGNORE_EXCEPTIONS as raven_exceptions
-
 # Shared settings across all environments
 
 def initialize_config(cls):
@@ -23,23 +21,14 @@ class Config(object):
 
     REDIS_PORT = 6379
     RQ_POLL_INTERVAL = 2000
-    RQ_DEFAULT_HOST = REDIS_HOST = CACHE_REDIS_HOST = \
+    RQ_DEFAULT_HOST = REDIS_HOST = \
         os.getenv('REDIS_HOST', 'localhost')
 
     STORAGE_SERVER = False
-
-    RAVEN_IGNORE_EXCEPTIONS = raven_exceptions
-
-    # Service Keys
-    GOOGLE = {
-        'consumer_key': os.environ.get('GOOGLE_ID'),
-        'consumer_secret':  os.environ.get('GOOGLE_SECRET')
-    }
-
-    SENDGRID_AUTH = {
-        'user': os.environ.get("SENDGRID_USER"),
-        'key': os.environ.get("SENDGRID_KEY")
-    }
+    STORAGE_PROVIDER = os.getenv('STORAGE_PROVIDER', 'LOCAL')
+    STORAGE_CONTAINER = os.environ.get('STORAGE_CONTAINER', os.path.abspath("./local-storage"))
+    STORAGE_KEY = os.environ.get('STORAGE_KEY', '')
+    STORAGE_SECRET = os.environ.get('STORAGE_SECRET', '').replace('\\n', '\n')
 
     @classmethod
     def initialize_config(cls):
@@ -63,12 +52,6 @@ class Config(object):
         if cls.SQLALCHEMY_DATABASE_URI.startswith(sqlite_prefix):
             cls.SQLALCHEMY_DATABASE_URI = (sqlite_prefix +
                     os.path.abspath(cls.SQLALCHEMY_DATABASE_URI[len(sqlite_prefix) + 1:]))
-
-
-        cls.STORAGE_PROVIDER = os.getenv('STORAGE_PROVIDER', 'LOCAL')
-        cls.STORAGE_CONTAINER = os.environ.get('STORAGE_CONTAINER', os.path.abspath("./local-storage"))
-        cls.STORAGE_KEY = os.environ.get('STORAGE_KEY', '')
-        cls.STORAGE_SECRET = os.environ.get('STORAGE_SECRET', '').replace('\\n', '\n')
 
         if cls.STORAGE_PROVIDER == 'LOCAL' and not os.path.exists(cls.STORAGE_CONTAINER):
             os.makedirs(cls.STORAGE_CONTAINER)

server/settings/_devbase.py
@@ -3,7 +3,6 @@
 class Config(object):
     ASSETS_DEBUG = True
     TESTING_LOGIN = True
-    INSTANTCLICK = True
 
     CACHE_TYPE = 'simple'
 

server/settings/_prodbase.py
@@ -1,5 +1,7 @@
 import os
 
+from server.settings import RAVEN_IGNORE_EXCEPTIONS as raven_exceptions
+
 # Shared settings across prod, staging and simple
 
 class Config(object):
@@ -11,12 +13,24 @@ class Config(object):
     WTF_CSRF_ENABLED = True
     
     CACHE_TYPE = 'simple'
-    CACHE_KEY_PREFIX = 'ok-web'
 
     RQ_DEFAULT_HOST = REDIS_HOST = CACHE_REDIS_HOST = \
         os.getenv('REDIS_HOST', 'redis-master')
 
     STORAGE_CONTAINER = os.environ.get('STORAGE_CONTAINER',  'ok-v3-user-files')
+
+    RAVEN_IGNORE_EXCEPTIONS = raven_exceptions
+
+    # Service Keys
+    GOOGLE = {
+        'consumer_key': os.environ.get('GOOGLE_ID'),
+        'consumer_secret':  os.environ.get('GOOGLE_SECRET')
+    }
+
+    SENDGRID_AUTH = {
+        'user': os.environ.get("SENDGRID_USER"),
+        'key': os.environ.get("SENDGRID_KEY")
+    }
 
     @classmethod
     def verify_oauth_credentials(cls):

server/settings/dev.py
@@ -6,8 +6,10 @@ from server.settings._devbase import Config as DevBaseConfig
 @initialize_config
 class Config(DevBaseConfig, BaseConfig):
     ENV = 'dev'
-    
+
     SECRET_KEY = os.getenv('OK_SESSION_KEY', 'changeinproductionkey')
+
+    INSTANTCLICK = True
 
     DEBUG = True
 

server/settings/prod.py
@@ -16,6 +16,7 @@ class Config(ProdBaseConfig, BaseConfig):
     MAX_CONTENT_LENGTH = 30 * 1024 * 1024  # Max Upload Size is 30MB
 
     CACHE_TYPE = 'redis'
+    CACHE_KEY_PREFIX = 'ok-web'
 
     # The Google Cloud load balancer behaves like two proxies: 
     # one with the external fowarding rule IP, and

server/settings/simple.py
@@ -10,10 +10,12 @@ from server.settings._prodbase import Config as ProdBaseConfig
 class Config(ProdBaseConfig, BaseConfig):
     ENV = 'simple'
 
-    PREFERRED_URL_SCHEME = 'http'
-
     RQ_DEFAULT_HOST = REDIS_HOST = CACHE_REDIS_HOST = \
         os.getenv('REDIS_HOST', 'localhost')
+
+    CACHE_KEY_PREFIX = 'ok-web'
+
+    STORAGE_CONTAINER = os.environ.get('STORAGE_CONTAINER', os.path.abspath("./local-storage"))
 
     @classmethod
     def get_default_db_url(cls):

server/settings/staging.py
@@ -12,6 +12,8 @@ class Config(ProdBaseConfig, BaseConfig):
 
     MAX_CONTENT_LENGTH = 10 * 1024 * 1024  # Max Upload Size is 10MB
 
+    STORAGE_CONTAINER = os.environ.get('STORAGE_CONTAINER', os.path.abspath("./local-storage"))
+
     @classmethod
     def get_default_db_url(cls):
         return os.getenv('SQLALCHEMY_URL', 'sqlite:///../oksqlite.db')