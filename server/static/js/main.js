c882ade40ef80b37551e1a4fe3f8fbee740fc3d0
Nancy <nancyshaw99@gmail.com>
2018-10-05 20:02:12 -0700

    custom icon test

--- server/static/js/main.js
+++ server/static/js/main.js
@@ -52,5 +52,16 @@ function debounce(func, wait, immediate) {
 };
 
 $('.datetime-picker').datetimepicker({
-  format: 'YYYY-MM-DD HH:mm:ss'
+  format: 'YYYY-MM-DD HH:mm:ss',
+  icons: {
+	time: 'glyphicon glyphicon-time',
+	date: 'glyphicon glyphicon-calendar',
+	up: 'glyphicon glyphicon-chevron-up',
+	down: 'glyphicon glyphicon-chevron-down',
+	previous: 'glyphicon glyphicon-chevron-left',
+	next: 'glyphicon glyphicon-chevron-right',
+	today: 'glyphicon glyphicon-screenshot',
+	clear: 'glyphicon glyphicon-trash',
+	close: 'glyphicon glyphicon-remove'
+  }
 });
